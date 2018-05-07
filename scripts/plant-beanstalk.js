1cddf4cbae2dfa6bf7692f4d4cff52b215f14041
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-05-07 11:15:19 +0100

    Make a simple express app for now

--- scripts/plant-beanstalk.js
+++ scripts/plant-beanstalk.js
@@ -10,7 +10,7 @@ if (!version) {
 const dockerrun = {
   "AWSEBDockerrunVersion": "1",
   "Image": {
-    "Name": "mojdigitalstudio/keyworker-ui:" + version,
+    "Name": "mojdigitalstudio/whereabouts:" + version,
     "Update": "true"
   },
   "Ports": [
