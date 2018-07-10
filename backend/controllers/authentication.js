15b99cf5d490b032f93046ba493583fc933b4d5f
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2018-07-10 18:59:10 +0100

    Login errors (#52)
    * identifying auth error type
    
    * NN-975 and NN-967 communicate login errors more precisely to user plus NEWB caseload missing error
    
    * NN-975 and NN-967 communicate login errors more precisely to user plus NEWB caseload missing error

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -24,32 +24,54 @@ router.get('/login', async (req, res) => {
     });
 });
 
-router.post('/login', async (req, res) => {
+router.post('/login', (req, res) => {
+  postLogin(req, res);
+});
+
+const postLogin = async (req, res) => {
   const healthRes = await health.healthResult();
   const isApiUp = (healthRes.status < 500);
   log.info(`loginIndex - health check called and the isAppUp = ${isApiUp}`);
   try {
     const response = await elite2Api.login(req);
-
     session.setHmppsCookie(res, response.data);
 
     res.redirect('/');
   } catch (error) {
     logError(req.url, error, 'Login failure');
+    let data = {
+      authError: true,
+      apiUp: isApiUp,
+      authErrorText: getAuthErrorDescription(error),
+      mailTo: mailTo,
+      homeLink: homeLink
+    };
+
     res.render(
       'login',
-      {
-        authError: true,
-        apiUp: isApiUp,
-        mailTo: mailTo,
-        homeLink: homeLink
-      });
+      data);
   }
-});
+};
+
+
+function getAuthErrorDescription (error) {
+  log.info(`login error description = ${error.response && error.response.data && error.response.data.error_description}`);
+  let type = 'The username or password you have entered is invalid.';
+  if (error.response && error.response.data && error.response.data.error_description) {
+    if (error.response.data.error_description.includes('ORA-28000')) {
+      type = 'Your user account is locked.';
+    } else if (error.response.data.error_description.includes('does not have access to caseload NWEB')) {
+      type = 'You are not enabled for this service, please contact admin and request access.';
+    } else if (error.response.data.error_description.includes('ORA-28001')) {
+      type = 'Your password has expired.';
+    }
+  }
+  return type;
+}
 
 router.get('/logout', (req, res) => {
   session.deleteHmppsCookie(res);
   res.redirect('/auth/login');
 });
 
-module.exports = router;
+module.exports = { router, postLogin };
