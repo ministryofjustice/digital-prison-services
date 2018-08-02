f235f188da2b9178d11fb44582fe4d60d363d314
JamieDraperUK <draper65@googlemail.com>
2018-08-02 11:24:11 +0100

    Merge branch 'master' into NN-1028-fix-ordering

--- backend/controllers/activityList.js
+++ backend/controllers/activityList.js
@@ -1,15 +1,46 @@
 const switchDateFormat = require('../utils');
 const log = require('../log');
 
+const getActivitiesGroupedByEventThenByLastName = (data) => {
+  const groupedByEvent = data.reduce((previous, current) => {
+    if (previous[current.comment]) {
+      previous[current.comment].push(current);
+    } else {
+      previous[current.comment] = [current];
+    }
+
+    return previous;
+  }, {});
+
+  const keys = Object.keys(groupedByEvent).sort();
+
+  if (keys.length) {
+    keys.forEach(key => {
+      const event = groupedByEvent[key];
+      event.sort((a, b) => {
+        if (a.lastName < b.lastName) return -1;
+        if (a.lastName > b.lastName) return 1;
+        return 0;
+      });
+    });
+
+    return keys.map(key => groupedByEvent[key]).reduce((previous, current) => current.concat(previous), []);
+  }
+
+  return null;
+};
+
 const getActivityListFactory = (elite2Api) => {
   const getActivityList = async (context, agencyId, locationId, frontEndDate, timeSlot) => {
     const date = switchDateFormat(frontEndDate);
 
     const sortFields = ['eventDescription', 'lastName'];
-    const activities = await elite2Api.getActivityList(context, { agencyId, locationId, usage: 'PROG', date, timeSlot, sortFields });
-    log.info(activities, 'getActivityList data received');
+    const activityData = await elite2Api.getActivityList(context, { agencyId, locationId, usage: 'PROG', date, timeSlot, sortFields });
+    log.info(activityData, 'getActivityList data received');
     const visits = await elite2Api.getActivityList(context, { agencyId, locationId, usage: 'VISIT', date, timeSlot });
     const appointments = await elite2Api.getActivityList(context, { agencyId, locationId, usage: 'APP', date, timeSlot });
+
+    const activities = getActivitiesGroupedByEventThenByLastName(activityData) || activityData;
 
     if (activities) {
       for (const row of activities) {
