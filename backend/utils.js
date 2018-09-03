a62d6694f03e28715d39b0902c0bbb43081a8091
JamieDraperUK <draper65@googlemail.com>
2018-09-03 15:26:15 +0100

    Address comments.

--- backend/utils.js
+++ backend/utils.js
@@ -1,19 +1,19 @@
 const moment = require('moment');
 
 //https://stackoverflow.com/questions/4340227/sort-mixed-alpha-numeric-array
-var reA = /[^a-zA-Z]/g;
-var reN = /[^0-9]/g;
+const alpha = /[^a-zA-Z]/g;
+const numeric = /[^0-9]/g;
 
 function sortAlphaNum (aa, bb) {
   const a = (aa || "");
   const b = (bb || "");
 
-  const aAlpha = a.replace(reA, "");
-  const bAlpha = b.replace(reA, "");
+  const aAlpha = a.replace(alpha, "");
+  const bAlpha = b.replace(alpha, "");
 
   if (aAlpha === bAlpha) {
-    const aNumber = parseInt(a.replace(reN, ""), 10);
-    const bNumber = parseInt(b.replace(reN, ""), 10);
+    const aNumber = parseInt(a.replace(numeric, ""), 10);
+    const bNumber = parseInt(b.replace(numeric, ""), 10);
     return aNumber === bNumber ? 0 : aNumber > bNumber ? 1 : -1; // eslint-disable-line no-nested-ternary
   } else {
     return aAlpha > bAlpha ? 1 : -1;
