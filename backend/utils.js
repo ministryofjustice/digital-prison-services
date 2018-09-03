7421a70cd1ffc9f1ea72f4ed2233ecac0c7801cf
JamieDraperUK <draper65@googlemail.com>
2018-09-03 14:42:15 +0100

    NN-1088 fix sorting in location list

--- backend/utils.js
+++ backend/utils.js
@@ -1,5 +1,25 @@
 const moment = require('moment');
 
+//https://stackoverflow.com/questions/4340227/sort-mixed-alpha-numeric-array
+var reA = /[^a-zA-Z]/g;
+var reN = /[^0-9]/g;
+
+function sortAlphaNum (aa, bb) {
+  const a = (aa || "");
+  const b = (bb || "");
+
+  const aAlpha = a.replace(reA, "");
+  const bAlpha = b.replace(reA, "");
+
+  if (aAlpha === bAlpha) {
+    const aNumber = parseInt(a.replace(reN, ""), 10);
+    const bNumber = parseInt(b.replace(reN, ""), 10);
+    return aNumber === bNumber ? 0 : aNumber > bNumber ? 1 : -1; // eslint-disable-line no-nested-ternary
+  } else {
+    return aAlpha > bAlpha ? 1 : -1;
+  }
+}
+
 const switchDateFormat = displayDate => {
   if (displayDate) {
     return moment(displayDate, 'DD/MM/YYYY').format('YYYY-MM-DD');
@@ -12,5 +32,6 @@ const distinct = (data) => data.reduce((accumulator, current) =>
 
 module.exports = {
   switchDateFormat,
-  distinct
+  distinct,
+  sortAlphaNum
 };
