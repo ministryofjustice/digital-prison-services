62531827092b783591f7ef065eaa7fb09d64d356
John Robertson <jsrobertson@gmail.com>
2018-10-03 12:07:03 +0100

    NN-1188 Added test for setCaseChangeRedirectStatus

--- src/redux/actions/actions.test.js
+++ src/redux/actions/actions.test.js
@@ -166,6 +166,15 @@ describe('actions', () => {
     expect(actions.setActivityData([{ stuff: 'stuff' }])).toEqual(expectedAction);
   });
 
+  it('should create an action to set case change redirect status', () => {
+    const bool = true;
+    const expectedAction = {
+      type: types.SET_CASE_CHANGE_REDIRECT,
+      bool
+    };
+    expect(actions.setCaseChangeRedirectStatus(bool)).toEqual(expectedAction);
+  });
+
   it('should create an action to set establishment roll block data', () => {
     const data = {
       movements: null,
