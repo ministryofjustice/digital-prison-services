c3e7e75ab69497eac515cf45dac53d604840af6e
John Robertson <jsrobertson@gmail.com>
2018-10-19 22:12:39 +0100

    NN-1285 Removed prefer-arrow-callback override

--- backend/index.js
+++ backend/index.js
@@ -165,7 +165,7 @@ app.get('*', (req, res) => {
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, function () {
+app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log('Backend running on port', port);
 });
