4de1da39fd5bf2bd5502ddd59cefc2c608d89ac6
Peter Phillips <peter@greenthistle.co.uk>
2018-10-23 15:48:33 +0100

    NN-1285: Enforce eslint function names

--- src/PreviousNextNavigation/style-utils.js
+++ src/PreviousNextNavigation/style-utils.js
@@ -12,7 +12,6 @@ export const media = Object.keys(sizes).reduce((accumulator, label) => {
   // use em in breakpoints to work properly cross-browser and support users
   // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
   const emSize = sizes[label] / 16
-  // eslint-disable-next-line
   accumulator[label] = (...args) => css`
     @media (min-width: ${emSize}em) {
       ${css(...args)};
