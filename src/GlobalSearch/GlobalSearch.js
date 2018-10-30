392cd5e59b711d1997eca5e5cc167871a80300f1
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2018-10-30 12:20:41 +0000

    Nn 1330 home breadcrumb missing from global search results page (#165)
    * NN-1330 Home breadcrumb missing from Global search results page
    
    * NN-1285: Improve global search readability after linting
    
    * Add waitFor's to fix integration test intermittent failures

--- src/GlobalSearch/GlobalSearch.js
+++ src/GlobalSearch/GlobalSearch.js
@@ -2,11 +2,19 @@ import React from 'react'
 import '../index.scss'
 import '../lists.scss'
 import '../App.scss'
+import links from '../links'
 import GlobalSearchResultList from './GlobalSearchResultList'
 
 const GlobalSearch = props => (
   <div>
-    <h1 className="heading-large whereabouts-title no-print">Global search results</h1>
+    <div className="padding-top">
+      <a title="Home" className="link" href={links.getHomeLink()}>
+        Home
+      </a>
+      <span> &gt; </span>
+      Global search results
+    </div>
+    <h1 className="heading-large">Global search results</h1>
     {/* TODO: 'search again' component goes here */}
     <GlobalSearchResultList {...props} />
   </div>
