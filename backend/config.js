0e4aa518199b4847762878e2807ce8299bb39bad
steverendell <steve.rendell@syscon.net>
2018-12-12 12:54:09 +0000

    NN-1486 Add an alert API endpoint to Elite2
    Add ability to use oauth clientId

--- backend/config.js
+++ backend/config.js
@@ -28,6 +28,8 @@ module.exports = {
       timeoutSeconds: process.env.API_ENDPOINT_TIMEOUT_SECONDS || 10,
       clientId: process.env.API_CLIENT_ID || 'elite2apiclient',
       clientSecret: process.env.API_CLIENT_SECRET || 'clientsecret',
+      systemClientId: process.env.API_SYSTEM_CLIENT_ID || 'prisonStaffHubClient',
+      systemClientSecret: process.env.API_SYSTEM_CLIENT_SECRET || 'clientsecret',
     },
     elite2: {
       url: process.env.API_ENDPOINT_URL || 'http://localhost:8080/',
--- backend/controllers/movementsService.js
+++ backend/controllers/movementsService.js
@@ -1,9 +1,13 @@
 const moment = require('moment')
+const oauthClientId = require('../api/oauthClientId')
 
 const movementsServiceFactory = elite2Api => {
   const getMovementsIn = (context, agencyId) => {
     const isoDateToday = moment().format('YYYY-MM-DD')
     return elite2Api.getMovementsIn(context, agencyId, isoDateToday)
+    // TODO getting flags etc:
+    // const systemContext = await oauthClientId.getClientCredentialsTokens()
+    // const stuff = await elite2Api.getAlertsSystem(systemContext, offenderNos)
   }
 
   return {
