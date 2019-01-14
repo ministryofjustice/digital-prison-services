af38d30cf0e90114d6b41ec9a7d0ce60d3b6c803
JamieDraperUK <draper65@googlemail.com>
2019-01-14 16:07:55 +0000

    NN-1557 in today result page fixes  - Fix key on offender results causing sorting to be an issue with new data  - Remove clickable link from offenders when their not in the current case load

--- src/MovementsIn/MovementsIn.test.js
+++ src/MovementsIn/MovementsIn.test.js
@@ -13,10 +13,46 @@ describe('Movements in', () => {
         location: 'MDI-1-3-017',
         movementTime: '12:01:02',
         fromAgencyDescription: 'Hull (HMP)',
+        toAgencyId: 'MDI',
         alerts: ['HA', 'XSA', 'XA', 'PEEP', 'XEL', 'XRF', 'XTACT'],
       },
     ]
-    const wrapper = shallow(<MovementsIn sortOrder="ASC" setColumnSort={jest.fn()} rows={movements} />)
+    const wrapper = shallow(<MovementsIn sortOrder="ASC" setColumnSort={jest.fn()} rows={movements} agencyId="MDI" />)
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should not make the offender clickable because there is no assigned location', () => {
+    const movements = [
+      {
+        offenderNo: 'G0000GG',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        movementTime: '12:01:02',
+        fromAgencyDescription: 'Hull (HMP)',
+        toAgencyCode: 'HLI',
+        alerts: ['HA', 'XSA', 'XA', 'PEEP', 'XEL', 'XRF', 'XTACT'],
+      },
+    ]
+    const wrapper = shallow(<MovementsIn sortOrder="ASC" setColumnSort={jest.fn()} rows={movements} agencyId="HLI" />)
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should not make the offender clickable because there their toAgencyId does not match to active caseload', () => {
+    const movements = [
+      {
+        offenderNo: 'G0000GG',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        movementTime: '12:01:02',
+        location: 'CELL-1',
+        fromAgencyDescription: 'Hull (HMP)',
+        toAgencyCode: 'HLI',
+        alerts: ['HA', 'XSA', 'XA', 'PEEP', 'XEL', 'XRF', 'XTACT'],
+      },
+    ]
+    const wrapper = shallow(<MovementsIn sortOrder="ASC" setColumnSort={jest.fn()} rows={movements} agencyId="LEI" />)
 
     expect(wrapper).toMatchSnapshot()
   })
