b32707a084611345896c99f6f0ac5d6cf2bdac77
Michael Willis <michael.willis@digital.justice.gov.uk>
2019-02-08 14:43:17 +0000

    New Repo for Docker

--- scripts/plant-beanstalk.js
+++ scripts/plant-beanstalk.js
@@ -10,7 +10,7 @@ if (!version) {
 const dockerrun = {
   AWSEBDockerrunVersion: '1',
   Image: {
-    Name: `mojdigitalstudio/whereabouts:${version}`,
+    Name: `mojdigitalstudio/prisonstaffhub:${version}`,
     Update: 'true',
   },
   Ports: [{ ContainerPort: '3000' }],
