161c17f8253e90b3cfe933ab51884916eff9922d
John Robertson <jsrobertson@gmail.com>
2019-02-14 19:13:09 +0000

    NN-1655 Update page titles

--- src/FooterContainer/FooterContainer.js
+++ src/FooterContainer/FooterContainer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
 import { Footer } from 'new-nomis-shared-components'
 import { fetchContentLinks } from '../redux/actions/index'
 
