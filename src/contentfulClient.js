55bf8de942157289e49e895bb5cfc930854f713f
John Robertson <jsrobertson@gmail.com>
2019-02-14 09:31:53 +0000

    Added contentful rich text renderer

---
+++ src/contentfulClient.js
@@ -0,0 +1,8 @@
+import * as contentful from 'contentful'
+
+const contentfulClient = contentful.createClient({
+  space: 'edstadadqo4d',
+  accessToken: 'abdc8ae3aa1f2c4101dc91c44d49314b979c2116e40ae8ec0ba36d24f103a01d',
+})
+
+export default contentfulClient
