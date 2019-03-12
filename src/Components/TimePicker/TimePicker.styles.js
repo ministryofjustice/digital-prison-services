f2d0fdad65f9288f95038d0614728bdb6ac282b3
John Robertson <jsrobertson@gmail.com>
2019-03-12 13:56:36 +0000

    NN-1728 Fix warnings

--- src/Components/TimePicker/TimePicker.styles.js
+++ src/Components/TimePicker/TimePicker.styles.js
@@ -1,5 +1,5 @@
 import styled from 'react-emotion'
-import { SPACING, BORDER_WIDTH_FORM_ELEMENT_ERROR } from '@govuk-react/constants'
+import { SPACING, BORDER_WIDTH_FORM_ELEMENT } from '@govuk-react/constants'
 import { ERROR_COLOUR } from 'govuk-colours'
 
 // eslint-disable-next-line import/prefer-default-export
@@ -13,7 +13,6 @@ export const Container = styled('div')`
   select {
     width: 95% !important;
     margin-right: ${SPACING.SCALE_3} !important;
-    border: ${props =>
-      props.error ? `${BORDER_WIDTH_FORM_ELEMENT_ERROR} solid ${ERROR_COLOUR}` : undefined} !important;
+    border: ${props => (props.error ? `${BORDER_WIDTH_FORM_ELEMENT} solid ${ERROR_COLOUR}` : undefined)} !important;
   }
 `
