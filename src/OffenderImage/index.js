46d514693bbd304c6b7289f8e37a9fc9e21fba5f
John Robertson <jsrobertson@gmail.com>
2019-03-25 15:18:45 +0000

    Fix offender image size regression

--- src/OffenderImage/index.js
+++ src/OffenderImage/index.js
@@ -1,14 +1,3 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-const OffenderImage = props => {
-  const { offenderNo } = props
-  const offenderImageUrl = `/app/images/${offenderNo}/data`
-  return <img id={`image-${offenderNo}`} alt={`prisoner ${offenderNo}`} className="photo" src={offenderImageUrl} />
-}
-
-OffenderImage.propTypes = {
-  offenderNo: PropTypes.string.isRequired,
-}
+import OffenderImage from './OffenderImage'
 
 export default OffenderImage
