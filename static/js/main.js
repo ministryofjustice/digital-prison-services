a490161ca26bba4bd16e6270a4bf1d0da9cc9b13
JamieDraperUK <draper65@googlemail.com>
2019-09-25 13:03:34 +0100

    NN-2171 fix future dates on date picker

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('.date-input').each((index, element) => {
-    var hideFutureDates = Boolean($(element).data('hide-future-date'))
+    var hideFutureDates = Boolean($(element).data('disable-future-date'))
     var maxDate = hideFutureDates ? '0' : undefined
 
     $(element).datepicker({
