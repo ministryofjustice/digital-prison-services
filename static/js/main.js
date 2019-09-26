2ba5ca80286f35e6bb56d2cbbf75e3407b6f99b0
JamieDraperUK <draper65@googlemail.com>
2019-09-26 16:13:37 +0100

    NN-2171 extend date picker to disable past dates

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $('.date-input').each((index, element) => {
-    var hideFutureDates = Boolean($(element).data('disable-future-date'))
-    var hidePastDates = Boolean($(element).data('disable-past-date'))
-    var maxDate = hideFutureDates ? '0' : undefined
-    var minDate = hidePastDates ? '0' : undefined
+    var disableFutureDates = Boolean($(element).data('disable-future-date'))
+    var disablePastDates = Boolean($(element).data('disable-past-date'))
+    var maxDate = disableFutureDates ? '0' : undefined
+    var minDate = disablePastDates ? '0' : undefined
 
     $(element).datepicker({
       dateFormat: 'dd/mm/yy',
