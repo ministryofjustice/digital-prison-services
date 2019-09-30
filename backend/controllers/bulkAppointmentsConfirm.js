53940d3524571d7a0285351cb21c980617c5ef24
John Robertson <jsrobertson@gmail.com>
2019-09-30 16:02:42 +0100

    NN-2175 Confirm appointments page with mixed appointments

--- backend/controllers/bulkAppointmentsConfirm.js
+++ backend/controllers/bulkAppointmentsConfirm.js
@@ -1,11 +1,57 @@
 const moment = require('moment')
-const { DAY_MONTH_YEAR, DATE_TIME_FORMAT_SPEC } = require('../../src/dateHelpers')
+const { DAY_MONTH_YEAR, DATE_TIME_FORMAT_SPEC, buildDateTime } = require('../../src/dateHelpers')
 
 const bulkAppointmentsConfirmFactory = (elite2Api, logError) => {
+  const renderTemplate = (req, res, pageData) => {
+    const { appointmentDetails, errors } = pageData
+
+    res.render('confirmAppointments.njk', {
+      appointmentDetails: {
+        ...appointmentDetails,
+        date: moment(appointmentDetails.date, DAY_MONTH_YEAR).format(DATE_TIME_FORMAT_SPEC),
+      },
+      errors,
+    })
+  }
+
   const renderError = (req, res, error) => {
     if (error) logError(req.originalUrl, error, 'Sorry, the service is unavailable')
 
     return res.render('error.njk', { url: '/bulk-appointments/need-to-upload-file' })
+  }
+
+  const validate = (prisonersWithAppointmentTimes, date) => {
+    const errors = []
+    const now = moment()
+    const isToday = date ? moment(date, DAY_MONTH_YEAR).isSame(now, 'day') : false
+
+    prisonersWithAppointmentTimes.forEach(({ startTime, endTime, offenderNo }) => {
+      const startTimeDuration = moment.duration(now.diff(startTime))
+      const endTimeDuration = endTime && moment.duration(moment(startTime).diff(endTime))
+
+      if (!startTime) {
+        errors.push({
+          text: `Select a start time for ${offenderNo}`,
+          href: `#${offenderNo}-start-time-hours`,
+        })
+      }
+
+      if (isToday && startTimeDuration.asMinutes() > 1) {
+        errors.push({
+          text: `Select a start time that is not in the past for ${offenderNo}`,
+          href: `#${offenderNo}-start-time-hours`,
+        })
+      }
+
+      if (endTime && endTimeDuration.asMinutes() > 1) {
+        errors.push({
+          text: `Select an end time that is not in the past for ${offenderNo}`,
+          href: `#${offenderNo}-end-time-hours`,
+        })
+      }
+    })
+
+    return errors
   }
 
   const index = async (req, res) => {
@@ -13,34 +59,67 @@ const bulkAppointmentsConfirmFactory = (elite2Api, logError) => {
 
     if (!data) return renderError(req, res)
 
-    const appointmentDetails = {
-      ...data,
-      date: moment(data.date, DAY_MONTH_YEAR).format(DATE_TIME_FORMAT_SPEC),
-    }
-
-    return res.render('confirmAppointments.njk', { appointmentDetails })
+    return renderTemplate(req, res, { appointmentDetails: data })
   }
 
   const post = async (req, res) => {
     const {
-      data: { appointmentType, location, startTime, endTime, prisonersListed, comment },
+      data: { appointmentType, location, startTime, endTime, date, prisonersListed, comment, sameTimeAppointments },
     } = req.session
 
-    const appointments = {
-      appointmentDefaults: {
-        comment,
-        locationId: Number(location),
-        appointmentType,
-        startTime,
-        endTime,
-      },
-      appointments: prisonersListed.map(prisoner => ({
-        bookingId: prisoner.bookingId,
-      })),
+    const prisonersWithAppointmentTimes = prisonersListed.map(prisoner => {
+      if (sameTimeAppointments === 'no') {
+        const startTimeHours = req.body[`${prisoner.offenderNo}startTimeHours`]
+        const startTimeMinutes = req.body[`${prisoner.offenderNo}startTimeMinutes`]
+        const endTimeHours = req.body[`${prisoner.offenderNo}endTimeHours`]
+        const endTimeMinutes = req.body[`${prisoner.offenderNo}endTimeMinutes`]
+
+        const startDateTime = buildDateTime({ date, hours: startTimeHours, minutes: startTimeMinutes })
+        const endDateTime = buildDateTime({ date, hours: endTimeHours, minutes: endTimeMinutes })
+
+        return {
+          ...prisoner,
+          startTimeHours,
+          startTimeMinutes,
+          endTimeHours,
+          endTimeMinutes,
+          startTime: startDateTime && startDateTime.format(DATE_TIME_FORMAT_SPEC),
+          endTime: endDateTime && endDateTime.format(DATE_TIME_FORMAT_SPEC),
+        }
+      }
+
+      return prisoner
+    })
+
+    if (sameTimeAppointments === 'no') {
+      const errors = validate(prisonersWithAppointmentTimes, date)
+
+      if (errors.length > 0) {
+        return renderTemplate(req, res, {
+          appointmentDetails: {
+            ...req.session.data,
+            prisonersListed: prisonersWithAppointmentTimes,
+          },
+          errors,
+        })
+      }
     }
 
     try {
-      await elite2Api.addBulkAppointments(res.locals, appointments)
+      await elite2Api.addBulkAppointments(res.locals, {
+        appointmentDefaults: {
+          comment,
+          locationId: Number(location),
+          appointmentType,
+          startTime,
+          endTime,
+        },
+        appointments: prisonersWithAppointmentTimes.map(prisoner => ({
+          bookingId: prisoner.bookingId,
+          startTime: prisoner.startTime,
+          endTime: prisoner.endTime,
+        })),
+      })
     } catch (error) {
       return renderError(req, res, error)
     }
--- backend/tests/bulkAppointmentsConfirm.test.js
+++ backend/tests/bulkAppointmentsConfirm.test.js
@@ -2,6 +2,22 @@ Reflect.deleteProperty(process.env, 'APPINSIGHTS_INSTRUMENTATIONKEY')
 const elite2Api = {}
 const { bulkAppointmentsConfirmFactory } = require('../controllers/bulkAppointmentsConfirm')
 
+let req
+let res
+
+beforeEach(() => {
+  elite2Api.addBulkAppointments = jest.fn()
+  req = {
+    originalUrl: '/bulk-appointments/confirm-appointment/',
+    session: {
+      userDetails: {
+        activeCaseLoadId: 'LEI',
+      },
+    },
+  }
+  res = { locals: {}, render: jest.fn(), redirect: jest.fn() }
+})
+
 describe('bulk appointments confirm', () => {
   const appointmentDetails = {
     appointmentType: 'TEST',
@@ -41,34 +57,24 @@ describe('bulk appointments confirm', () => {
     ],
   }
 
-  const mockReq = {
-    originalUrl: '/bulk-appointments/confirm-appointment/',
-  }
-
-  const mockRes = { render: jest.fn(), redirect: jest.fn(), locals: {} }
-
   const logError = jest.fn()
 
   const controller = bulkAppointmentsConfirmFactory(elite2Api, logError)
 
-  describe('view', () => {
-    describe('when there are no errors', () => {
-      const req = { ...mockReq, session: { data: { ...appointmentDetails } } }
-      const res = { ...mockRes }
+  describe('when viewing confirm appointments', () => {
+    describe('and there is data', () => {
+      it('should render the confirm appointments page', async () => {
+        req.session.data = { ...appointmentDetails }
 
-      it('should render the confirm appointments confirm page', async () => {
         await controller.index(req, res)
 
         expect(res.render).toBeCalledWith('confirmAppointments.njk', {
-          appointmentDetails: { ...appointmentDetails, date: '2019-09-23T00:00:00' },
+          appointmentDetails: { ...req.session.data, date: '2019-09-23T00:00:00' },
         })
       })
     })
 
-    describe('when there is an error', () => {
-      const req = { ...mockReq, session: {} }
-      const res = { ...mockRes }
-
+    describe('and there is no data', () => {
       it('should render the error page', async () => {
         await controller.index(req, res)
 
@@ -77,21 +83,18 @@ describe('bulk appointments confirm', () => {
     })
   })
 
-  describe('submit', () => {
-    const req = { ...mockReq, session: { data: { ...appointmentDetails } } }
-    const res = { ...mockRes }
+  describe('when submitting confirm appointments', () => {
+    describe('and the start times are the same', () => {
+      describe('and there are no errors', () => {
+        beforeEach(() => {
+          req.session.data = { ...appointmentDetails }
+          elite2Api.addBulkAppointments = jest.fn().mockReturnValue('All good')
+        })
 
-    describe('when there are no errors', () => {
-      beforeEach(() => {
-        elite2Api.addBulkAppointments = jest.fn().mockReturnValue('All good')
-      })
+        it('should submit the data and redirect to success page', async () => {
+          await controller.post(req, res)
 
-      it('should submit the data and redirect to success page', async () => {
-        await controller.post(req, res)
-
-        expect(elite2Api.addBulkAppointments).toBeCalledWith(
-          {},
-          {
+          expect(elite2Api.addBulkAppointments).toBeCalledWith(res.locals, {
             appointmentDefaults: {
               appointmentType: 'TEST',
               locationId: 1,
@@ -105,18 +108,139 @@ describe('bulk appointments confirm', () => {
               { bookingId: 'V38608' },
               { bookingId: 'V31474' },
             ],
-          }
-        )
+          })
 
-        expect(res.redirect).toBeCalledWith('/bulk-appointments/success')
+          expect(res.redirect).toBeCalledWith('/bulk-appointments/success')
+        })
       })
     })
 
-    describe('when there is an error', () => {
+    describe('are there are individual start and end times', () => {
+      beforeEach(() => {
+        elite2Api.addBulkAppointments = jest.fn().mockReturnValue('All good')
+        req.session.data = {
+          appointmentType: 'TEST',
+          location: 1,
+          date: '27/09/2019',
+          sameTimeAppointments: 'no',
+          comment: 'Activity comment',
+          prisonersNotFound: [],
+          prisonersListed: [
+            {
+              bookingId: 'K00278',
+              offenderNo: 'G1683VN',
+              firstName: 'Elton',
+              lastName: 'Abbatiello',
+            },
+            {
+              bookingId: 'V37486',
+              offenderNo: 'G4803UT',
+              firstName: 'Bobby',
+              lastName: 'Abdulkadir',
+            },
+          ],
+        }
+      })
+
+      describe('and there are no form errors', () => {
+        it('should submit the data and redirect to success page', async () => {
+          req.body = {
+            G1683VNstartTimeHours: '08',
+            G1683VNstartTimeMinutes: '30',
+            G1683VNendTimeHours: '',
+            G1683VNendTimeMinutes: '',
+            G4803UTstartTimeHours: '10',
+            G4803UTstartTimeMinutes: '00',
+            G4803UTendTimeHours: '',
+            G4803UTendTimeMinutes: '',
+          }
+
+          await controller.post(req, res)
+
+          expect(elite2Api.addBulkAppointments).toBeCalledWith(res.locals, {
+            appointmentDefaults: {
+              appointmentType: 'TEST',
+              comment: 'Activity comment',
+              locationId: 1,
+              startTime: undefined,
+              endTime: undefined,
+            },
+            appointments: [
+              { bookingId: 'K00278', startTime: '2019-09-27T08:30:00', endTime: '' },
+              { bookingId: 'V37486', startTime: '2019-09-27T10:00:00', endTime: '' },
+            ],
+          })
+
+          expect(res.redirect).toBeCalledWith('/bulk-appointments/success')
+        })
+      })
+
+      describe('and there are form errors ', () => {
+        it('should not submit the data and return with correct error messages', async () => {
+          req.body = {
+            G1683VNstartTimeHours: '08',
+            G1683VNstartTimeMinutes: '30',
+            G1683VNendTimeHours: '08',
+            G1683VNendTimeMinutes: '00',
+            G4803UTstartTimeHours: '',
+            G4803UTstartTimeMinutes: '',
+            G4803UTendTimeHours: '',
+            G4803UTendTimeMinutes: '',
+          }
+
+          await controller.post(req, res)
+
+          expect(elite2Api.addBulkAppointments).not.toBeCalled()
+          expect(res.render).toBeCalledWith('confirmAppointments.njk', {
+            appointmentDetails: {
+              appointmentType: 'TEST',
+              location: 1,
+              sameTimeAppointments: 'no',
+              comment: 'Activity comment',
+              prisonersNotFound: [],
+              prisonersListed: [
+                {
+                  bookingId: 'K00278',
+                  startTime: '2019-09-27T08:30:00',
+                  startTimeHours: '08',
+                  startTimeMinutes: '30',
+                  endTime: '2019-09-27T08:00:00',
+                  endTimeHours: '08',
+                  endTimeMinutes: '00',
+                  firstName: 'Elton',
+                  lastName: 'Abbatiello',
+                  offenderNo: 'G1683VN',
+                },
+                {
+                  bookingId: 'V37486',
+                  startTime: '',
+                  startTimeHours: '',
+                  startTimeMinutes: '',
+                  endTime: '',
+                  endTimeHours: '',
+                  endTimeMinutes: '',
+                  firstName: 'Bobby',
+                  lastName: 'Abdulkadir',
+                  offenderNo: 'G4803UT',
+                },
+              ],
+              date: '2019-09-27T00:00:00',
+            },
+            errors: [
+              { href: '#G1683VN-end-time-hours', text: 'Select an end time that is not in the past for G1683VN' },
+              { href: '#G4803UT-start-time-hours', text: 'Select a start time for G4803UT' },
+            ],
+          })
+        })
+      })
+    })
+
+    describe('when there is an issue with the api', () => {
       beforeEach(() => {
         elite2Api.addBulkAppointments = jest.fn().mockImplementation(() => {
           throw new Error('There has been an error')
         })
+        req.session.data = { ...appointmentDetails }
       })
 
       it('should log an error and render the error page', async () => {
