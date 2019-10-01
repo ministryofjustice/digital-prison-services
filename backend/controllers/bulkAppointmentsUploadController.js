e6d9f334bfccbbf025447c899636ea5bd83f8db6
Rob Cooper <coops24@gmail.com>
2019-10-01 13:23:26 +0100

    Nn 2172 correct error messages for file upload (#557)
    * NN-2172 updated error messaging and tests
    
    * Updated view to handle non existing parameters
    
    * NN-2172 corrected file download link
    
    * NN-2172 updated error messages to match ACs
    
    * NN-2172 added error messaging for no file and empty file
    
    * NN-2172 readded rogue test for empty file

---
+++ backend/controllers/bulkAppointmentsUploadController.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const { isBinaryFileSync } = require('isbinaryfile')
+const fs = require('fs')
+const { bulkAppointmentsUploadFactory } = require('./bulkAppointmentsUpload')
+const { offenderLoaderFactory } = require('../controllers/offender-loader')
+const { csvParserService } = require('../csv-parser')
+
+const router = express.Router()
+
+const controller = ({ elite2Api, logError }) => {
+  const { index, post } = bulkAppointmentsUploadFactory(
+    csvParserService({ fs, isBinaryFileSync }),
+    offenderLoaderFactory(elite2Api),
+    logError
+  )
+
+  router.get('/', index)
+  router.post('/', post)
+
+  return router
+}
+
+module.exports = dependencies => controller(dependencies)
