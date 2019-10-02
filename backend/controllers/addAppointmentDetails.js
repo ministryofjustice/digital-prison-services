949a3446321b008e7b1fdf45c68f3dfd580884f1
JamieDraperUK <draper65@googlemail.com>
2019-10-02 13:07:30 +0100

    NN-2186 recurring appointments

--- backend/controllers/addAppointmentDetails.js
+++ backend/controllers/addAppointmentDetails.js
@@ -3,6 +3,8 @@ const config = require('../config')
 const { serviceUnavailableMessage } = require('../common-messages')
 const { DATE_TIME_FORMAT_SPEC, DAY_MONTH_YEAR } = require('../../src/dateHelpers')
 
+const { calculateEndDate } = require('../../src/BulkAppointments/RecurringAppointments')
+
 const {
   app: { notmEndpointUrl },
 } = config
@@ -25,7 +27,13 @@ const setSelected = (value, items) =>
 
 const buildDateTime = ({ date, hours, minutes }) => {
   const time = date && hours && minutes && moment(date, DAY_MONTH_YEAR)
-  return time && time.hour(Number(hours)).minutes(Number(minutes))
+  return (
+    time &&
+    time
+      .hour(Number(hours))
+      .minutes(Number(minutes))
+      .seconds(0)
+  )
 }
 
 const extractHoursMinutes = dateTime => {
@@ -37,6 +45,33 @@ const extractHoursMinutes = dateTime => {
   }
 }
 
+const endRecurringEndingDate = ({ date, startTime, times, repeats }) => {
+  const recurringStartTime =
+    (startTime && moment(startTime)) ||
+    moment(date, DAY_MONTH_YEAR)
+      .hours(0)
+      .minutes(0)
+
+  const endOfPeriod = calculateEndDate({
+    startTime: recurringStartTime,
+    repeats,
+    numberOfTimes: times,
+  })
+
+  return {
+    endOfPeriod,
+    recurringStartTime,
+  }
+}
+
+const repeatTypes = [
+  { value: 'WEEKLY', text: 'Weekly' },
+  { value: 'DAILY', text: 'Daily' },
+  { value: 'WEEKDAYS', text: 'Weekday (Monday to Friday)' },
+  { value: 'MONTHLY', text: 'Monthly' },
+  { value: 'FORTNIGHTLY', text: 'Fortnightly' },
+]
+
 const getValidationMessages = ({
   appointmentType,
   location,
@@ -45,14 +80,20 @@ const getValidationMessages = ({
   endTime,
   sameTimeAppointments,
   comments,
+  recurring,
+  repeats,
+  times,
 }) => {
   const errors = []
   const now = moment()
   const isToday = date ? moment(date, DAY_MONTH_YEAR).isSame(now, 'day') : false
 
   if (!appointmentType) errors.push({ text: 'Select an appointment type', href: '#appointment-type' })
+
   if (!location) errors.push({ text: 'Select a location', href: '#location' })
+
   if (!date) errors.push({ text: 'Select a date', href: '#date' })
+
   if (!sameTimeAppointments)
     errors.push({ text: 'Select yes if the appointments all have the same time', href: '#same-time-appointments' })
 
@@ -79,6 +120,38 @@ const getValidationMessages = ({
   if (comments && comments.length > 3600)
     errors.push({ text: 'Maximum length should not exceed 3600 characters', href: '#comments' })
 
+  if (!recurring) errors.push({ href: '#recurring', text: 'Select yes if these are recurring appointments' })
+  if (recurring === 'yes' && !repeats) errors.push({ href: '#repeats', text: 'Select a period' })
+
+  if (recurring === 'yes') {
+    if (!times) errors.push({ href: '#times', text: 'Number of occurrences must be 1 or more' })
+    if (!Number.isInteger(Number(times))) errors.push({ href: '#times', text: 'Please enter a number' })
+
+    if (repeats && times) {
+      const { recurringStartTime, endOfPeriod } = endRecurringEndingDate({ date, startTime, repeats, times })
+
+      if (endOfPeriod && endOfPeriod.isSameOrAfter(recurringStartTime.startOf('day').add(1, 'years'))) {
+        errors.push({
+          href: '#times',
+          text: 'Select fewer number of appointments - you can only add them for a maximum of 1 year',
+        })
+      }
+    }
+
+    if (repeats === 'WEEKDAYS') {
+      const SATURDAY = 6
+      const SUNDAY = 0
+      if (moment(date, DAY_MONTH_YEAR).day() === SATURDAY || moment(date, DAY_MONTH_YEAR).day() === SUNDAY) {
+        errors.push({
+          href: '#date',
+          text: 'The date must be a week day',
+        })
+      }
+    }
+
+    if (Number(times) <= 0) errors.push({ href: '#times', text: 'Enter how many appointments you want to add' })
+  }
+
   return errors
 }
 
@@ -100,8 +173,18 @@ const addAppointmentDetailsFactory = (bulkAppointmentService, oauthApi, logError
       const { activeCaseLoadId } = req.session.userDetails
       const { appointmentTypes, locations } = await getAppointmentTypesAndLocations(res.locals, activeCaseLoadId)
 
-      const { appointmentType, location, date, startTime, endTime, sameTimeAppointments, comments } =
-        req.session.data || {}
+      const {
+        appointmentType,
+        location,
+        date,
+        startTime,
+        endTime,
+        sameTimeAppointments,
+        comments,
+        recurring,
+        times,
+        repeats,
+      } = req.session.data || {}
 
       const startTimeHoursMinutes = extractHoursMinutes(startTime)
       const endTimeHoursMinutes = extractHoursMinutes(endTime)
@@ -120,6 +203,10 @@ const addAppointmentDetailsFactory = (bulkAppointmentService, oauthApi, logError
         endTimeHours: endTimeHoursMinutes.hours,
         endTimeMinutes: endTimeHoursMinutes.minutes,
         sameTimeAppointments,
+        repeatTypes,
+        recurring,
+        repeats,
+        times,
       })
     } catch (error) {
       logError(req.originalUrl, error, serviceUnavailableMessage)
@@ -143,6 +230,9 @@ const addAppointmentDetailsFactory = (bulkAppointmentService, oauthApi, logError
         endTimeMinutes,
         sameTimeAppointments,
         comments,
+        times,
+        repeats,
+        recurring,
       } = req.body || {}
 
       const { appointmentTypes, locations } = await getAppointmentTypesAndLocations(res.locals, activeCaseLoadId)
@@ -161,13 +251,24 @@ const addAppointmentDetailsFactory = (bulkAppointmentService, oauthApi, logError
         endTime,
         sameTimeAppointments,
         comments,
+        times,
+        repeats,
+        recurring,
       })
+
+      const { endOfPeriod } = endRecurringEndingDate({ date, startTime, repeats, times })
 
       if (!errors.length) {
         const timeInfo = sameTimeAppointments === 'yes' && {
           startTime: startTime && startTime.format(DATE_TIME_FORMAT_SPEC),
           endTime: endTime && endTime.format(DATE_TIME_FORMAT_SPEC),
         }
+
+        const recurringInfo = recurring === 'yes' && {
+          times,
+          repeats,
+        }
+
         // eslint-disable-next-line no-param-reassign
         req.session.data = {
           appointmentType,
@@ -177,26 +278,33 @@ const addAppointmentDetailsFactory = (bulkAppointmentService, oauthApi, logError
           date,
           sameTimeAppointments,
           comments,
+          recurring,
           ...timeInfo,
+          ...recurringInfo,
         }
         res.redirect('/bulk-appointments/upload-file')
         return
       }
 
       res.render('addAppointmentDetails.njk', {
+        errors,
         date,
         sameTimeAppointments,
         startTimeHours,
         startTimeMinutes,
         endTimeHours,
         endTimeMinutes,
-        errors,
         comments,
         appointmentTypes: setSelected(appointmentType, appointmentTypes),
         locations: setSelected(Number(location), locations),
         appointmentType,
         location: Number(location),
         homeUrl: notmEndpointUrl,
+        repeatTypes,
+        repeats,
+        times: Number(times),
+        recurring,
+        endOfPeriod: endOfPeriod && endOfPeriod.format('dddd, MMMM Do YYYY'),
       })
     } catch (error) {
       logError(req.originalUrl, error, serviceUnavailableMessage)
