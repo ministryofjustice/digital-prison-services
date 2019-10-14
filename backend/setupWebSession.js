69f503b2e421c01fa309ec41982ee7a5479d5f0e
JamieDraperUK <draper65@googlemail.com>
2019-10-14 13:07:31 +0100

    NN-2203 introduce redis and docker compose

--- backend/setupWebSession.js
+++ backend/setupWebSession.js
@@ -1,30 +1,41 @@
 const express = require('express')
-const cookieSession = require('cookie-session')
+const redis = require('redis')
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
+
 const config = require('./config')
 
 const router = express.Router()
 
 module.exports = () => {
+  const getSessionStore = () => {
+    const { host, port, password } = config.redis
+    if (!host) return null
+
+    const passwordOnly = password ? `:${password}` : ''
+    const url = `redis://${passwordOnly}@${host}:${port}`
+
+    const client = redis.createClient(url)
+
+    return new RedisStore({ client })
+  }
+
   router.use(
-    cookieSession({
-      name: config.hmppsCookie.name,
-      domain: config.hmppsCookie.domain,
-      maxAge: config.hmppsCookie.expiryMinutes * 60 * 1000,
-      secure: config.app.production,
-      httpOnly: true,
-      signed: true,
-      keys: [config.hmppsCookie.sessionSecret],
-      overwrite: true,
-      sameSite: 'lax',
+    session({
+      store: getSessionStore(),
+      secret: [config.hmppsCookie.sessionSecret],
+      resave: false,
+      saveUninitialized: false,
+      rolling: true,
+      cookie: {
+        domain: config.hmppsCookie.domain,
+        httpOnly: true,
+        maxAge: config.hmppsCookie.expiryMinutes * 60 * 1000,
+        sameSite: 'strict',
+        secure: config.app.production,
+        signed: true,
+      },
     })
   )
-
-  // Ensure cookie session is extended (once per minute) when user interacts with the server
-  router.use((req, res, next) => {
-    // eslint-disable-next-line no-param-reassign
-    req.session.nowInMinutes = Math.floor(Date.now() / 60e3)
-    next()
-  })
-
   return router
 }
