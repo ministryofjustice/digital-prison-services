cb8958212fa1dad5680b9e69c2d132a0ecf4c277
John Robertson <jsrobertson@gmail.com>
2019-10-15 12:02:58 +0100

    NN-2219 Add int test for removing clashing appointments

--- prisonstaffhub-specs/src/test/groovy/uk/gov/justice/digital/hmpps/prisonstaffhub/specs/BulkAppointmentsSpecification.groovy
+++ prisonstaffhub-specs/src/test/groovy/uk/gov/justice/digital/hmpps/prisonstaffhub/specs/BulkAppointmentsSpecification.groovy
@@ -1,14 +1,19 @@
 package uk.gov.justice.digital.hmpps.prisonstaffhub.specs
 
 import geb.Browser
+import groovy.json.JsonOutput
 import org.junit.Rule
 import uk.gov.justice.digital.hmpps.prisonstaffhub.mockapis.Elite2Api
 import uk.gov.justice.digital.hmpps.prisonstaffhub.mockapis.OauthApi
+import uk.gov.justice.digital.hmpps.prisonstaffhub.mockapis.mockResponses.AppointmentsResponse
+import uk.gov.justice.digital.hmpps.prisonstaffhub.mockapis.mockResponses.VisitsResponse
 import uk.gov.justice.digital.hmpps.prisonstaffhub.model.TestFixture
 import uk.gov.justice.digital.hmpps.prisonstaffhub.model.UserAccount
 import uk.gov.justice.digital.hmpps.prisonstaffhub.pages.BulkAppointmentsAddPage
 import uk.gov.justice.digital.hmpps.prisonstaffhub.pages.BulkAppointmentsAddedPage
+import uk.gov.justice.digital.hmpps.prisonstaffhub.pages.BulkAppointmentsClashesPage
 import uk.gov.justice.digital.hmpps.prisonstaffhub.pages.BulkAppointmentsConfirmPage
+import uk.gov.justice.digital.hmpps.prisonstaffhub.pages.BulkAppointmentsNotAddedPage
 import uk.gov.justice.digital.hmpps.prisonstaffhub.pages.BulkAppointmentsUploadCSVPage
 
 import java.time.LocalDate
@@ -110,6 +115,57 @@ class BulkAppointmentsSpecification extends BrowserReportingSpec {
 
         then: "I am presented with the appointments added page"
         at BulkAppointmentsAddedPage
+    }
+
+    def "should not add any appointments when all have been removed due to clashes" () {
+        setupTests()
+
+        def offenderNos = ['A12345']
+        elite2api.stubExternalTransfers(UserAccount.ITAG_USER.workingCaseload, offenderNos, startDate.toString(), true)
+        elite2api.stubCourtEvents(UserAccount.ITAG_USER.workingCaseload, offenderNos, startDate.toString(), true)
+        elite2api.stubAppointments(UserAccount.ITAG_USER.workingCaseload, "", startDate.toString(), offenderNos, AppointmentsResponse.appointments)
+        elite2api.stubVisits(UserAccount.ITAG_USER.workingCaseload, "", startDate.toString(), offenderNos, VisitsResponse.visits)
+
+        given: "I navigate to the add bulk appointments screen"
+        to BulkAppointmentsAddPage
+
+        when: "I fill out the appointment details"
+        at BulkAppointmentsAddPage
+        enterBasicAppointmentDetails(startDate.format(shortDatePattern))
+        form.sameTimeAppointments = "yes"
+        form.startTimeHours = 10
+        form.startTimeMinutes = 10
+        form.recurring = "no"
+        form.comments = "Test comment."
+
+        and: "I submit"
+        submitButton.click()
+
+        and: "I upload a CSV"
+        at BulkAppointmentsUploadCSVPage
+        selectFile()
+        submitButton.click()
+
+        then: "I am on the confirm appointments page"
+        at BulkAppointmentsConfirmPage
+
+        and: "I confirm the appointments I want to create"
+        submitButton.click()
+
+        then: "I am presented with the appointment clashes page"
+        at BulkAppointmentsClashesPage
+        prisonersWithClashes.children()[1].text() ==
+                "A12345 Doe 10:10\n" +
+                "Visiting room - Visits - 18:00 to 18:30\n" +
+                "Medical Room1 - Medical - Dentist - 15:30"
+
+        when: "I select that I would like to remove the appointments and confirm"
+        form.A12345 = 'remove'
+        submitButton.click()
+
+        then: "I am presented with the no appointments have been added page"
+        at BulkAppointmentsNotAddedPage
+        notAddedMessage == "This is because you have removed all the appointments which clashed."
     }
 
     def shortDatePattern = DateTimeFormatter.ofPattern("d/M/yyyy")
