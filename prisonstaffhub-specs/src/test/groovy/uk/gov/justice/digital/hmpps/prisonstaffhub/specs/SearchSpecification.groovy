a7891e33eba1ad4608eb81835b7a7a1e017bdaee
Nevelina Aleksandrova <nevelina@diginaga.online>
2019-11-15 14:00:58 +0000

    NN-2246: Remove now irrelevant e2e tests

--- prisonstaffhub-specs/src/test/groovy/uk/gov/justice/digital/hmpps/prisonstaffhub/specs/SearchSpecification.groovy
+++ prisonstaffhub-specs/src/test/groovy/uk/gov/justice/digital/hmpps/prisonstaffhub/specs/SearchSpecification.groovy
@@ -73,25 +73,4 @@ class SearchSpecification extends BrowserReportingSpec {
         then: 'a new activity location list is displayed'
         activity.find('option', value: '4').text() == 'loc4'
     }
-
-    def "Then the caseload is changed the search page is updated with the new locations"() {
-
-        given: 'I am on the search page'
-        fixture.toSearch()
-        at SearchPage
-
-        when: 'The active caseload is changed'
-        elite2api.stubHealth()
-        elite2api.stubUpdateActiveCaseload()
-        elite2api.stubGroups Caseload.SYI
-        oauthApi.stubGetMyDetails ITAG_USER
-        oauthApi.stubGetMyRoles()
-        elite2api.stubGetMyCaseloads(ITAG_USER.caseloads, Caseload.SYI.id)
-        elite2api.stubActivityLocations()
-        header.switchCaseload(Caseload.SYI.id);
-
-        then: 'the locations LOV is updated'
-        waitFor { $("div", text: contains("SHREWSBURY (HMP)")) }
-        location.find('option', value: 'block1').text() == 'block1'
-    }
 }
