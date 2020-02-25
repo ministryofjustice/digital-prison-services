fc5dd692f270c6c7bb0652aace7ca7162df37da3
John Robertson <jsrobertson@gmail.com>
2020-02-25 15:15:57 +0000

    NN-2387 Add appointments filter form

--- backend/controllers/appointments/viewAppointments.js
+++ backend/controllers/appointments/viewAppointments.js
@@ -1,10 +1,42 @@
+const moment = require('moment')
+const { serviceUnavailableMessage } = require('../../common-messages')
+
 const viewAppointmentsFactory = (elite2Api, whereaboutsApi, logError) => {
   const index = async (req, res) => {
-    return res.render('viewAppointments.njk', {})
+    const { date = moment().format('DD/MM/YYYY'), period, type, location } = req.query
+
+    try {
+      const [appointmentTypes, appointmentLocations] = await Promise.all([
+        elite2Api.getAppointmentTypes(res.locals),
+        elite2Api.getLocationsForAppointments(res.locals, req.session.userDetails.activeCaseLoadId),
+      ])
+
+      const types = appointmentTypes.map(appointmentType => ({
+        text: appointmentType.description,
+        value: appointmentType.code,
+      }))
+
+      const locations = appointmentLocations.map(appointmentLocation => ({
+        text: appointmentLocation.userDescription,
+        value: appointmentLocation.locationId,
+      }))
+
+      return res.render('viewAppointments.njk', {
+        types,
+        locations,
+        date: moment(date, 'DD/MM/YYYY').format('DD/MM/YYYY'),
+        formattedDate: moment(date, 'DD/MM/YYYY').format('D MMMM YYYY'),
+        period: period || 'am',
+        type: type || 'all',
+        location: location || 'all',
+      })
+    } catch (error) {
+      logError(req.originalUrl, error, serviceUnavailableMessage)
+      return res.render('error.njk')
+    }
   }
 
-  const post = async (req, res) => {}
-  return { index, post }
+  return { index }
 }
 
 module.exports = viewAppointmentsFactory
--- backend/controllers/appointments/viewAppointmentsController.js
+++ backend/controllers/appointments/viewAppointmentsController.js
@@ -4,10 +4,9 @@ const viewAppointmentsFactory = require('./viewAppointments')
 const router = express.Router({ mergeParams: true })
 
 const controller = ({ elite2Api, whereaboutsApi, logError }) => {
-  const { index, post } = viewAppointmentsFactory(elite2Api, whereaboutsApi, logError)
+  const { index } = viewAppointmentsFactory(elite2Api, whereaboutsApi, logError)
 
   router.get('/', index)
-  router.post('/', post)
 
   return router
 }
