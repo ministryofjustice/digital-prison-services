90a867d20067f11ea5f4e47a79a1cefc99e2b78d
John Robertson <jsrobertson@users.noreply.github.com>
2020-03-06 17:46:10 +0000

    NN-2387 Some integration tests for appointments page (#817)

---
+++ prisonstaffhub-specs/src/test/groovy/uk/gov/justice/digital/hmpps/prisonstaffhub/specs/ViewAppointmentsSpecification.groovy
@@ -0,0 +1,160 @@
+package uk.gov.justice.digital.hmpps.prisonstaffhub.specs
+
+import org.junit.Rule
+import uk.gov.justice.digital.hmpps.prisonstaffhub.mockapis.Elite2Api
+import uk.gov.justice.digital.hmpps.prisonstaffhub.mockapis.OauthApi
+import uk.gov.justice.digital.hmpps.prisonstaffhub.mockapis.WhereaboutsApi
+import uk.gov.justice.digital.hmpps.prisonstaffhub.model.TestFixture
+import uk.gov.justice.digital.hmpps.prisonstaffhub.model.UserAccount
+import uk.gov.justice.digital.hmpps.prisonstaffhub.pages.ViewAppointmentsPage
+
+import static uk.gov.justice.digital.hmpps.prisonstaffhub.model.UserAccount.ITAG_USER
+
+class ViewAppointmentsSpecification extends BrowserReportingSpec {
+    @Rule
+    Elite2Api elite2api = new Elite2Api()
+
+    @Rule
+    OauthApi oauthApi = new OauthApi()
+
+    @Rule
+    WhereaboutsApi whereaboutsApi = new WhereaboutsApi()
+
+    TestFixture fixture = new TestFixture(browser, elite2api, oauthApi, whereaboutsApi)
+
+    def "should display no results"() {
+        setupTests()
+
+        elite2api.stubGetAppointmentsForAgency(UserAccount.ITAG_USER.workingCaseload.id, [])
+        whereaboutsApi.getVideoLinkAppointments([])
+
+        when: "I navigate to the view appointments page"
+        to ViewAppointmentsPage
+
+        then: "I should be told that there are no results to display"
+        noResultsMessage.displayed
+    }
+
+    def "should display correct results"() {
+        setupTestsWithData()
+
+        when: "I navigate to the view appointments page"
+        to ViewAppointmentsPage
+
+        then: "I should be presented with the correct results"
+        searchResultsTableRows[1].text() == '12:30 One, Offender ABC123 Medical - Other HEALTH CARE Staff One'
+        searchResultsTableRows[2].text() == '13:30 to 14:30 Two, Offender ABC456 Gym - Exercise GYM Staff Two'
+        searchResultsTableRows[3].text() == '14:30 to 15:30 Three, Offender ABC789 Video Link booking VCC ROOM Wimbledon'
+    }
+
+    def "should filter by appointment type"() {
+        setupTestsWithData()
+
+        when: "I navigate to the view appointments page"
+        to ViewAppointmentsPage
+
+        and: "I select Video link booking and update results"
+        form.type = 'VLB'
+        submitButton.click()
+
+        then: "I should be presented with correct results"
+        at ViewAppointmentsPage
+        searchResultsTableRows.size() == 2
+        searchResultsTableRows[1].text() == '14:30 to 15:30 Three, Offender ABC789 Video Link booking VCC ROOM Wimbledon'
+    }
+
+    def setupTests() {
+        fixture.loginAs(ITAG_USER)
+        elite2api.stubAppointmentTypes([Map.of("code", "ACTI", "description", "Activities"), Map.of("code", "VLB", "description", "Video link booking")], )
+        elite2api.stubAppointmentLocations(
+                UserAccount.ITAG_USER.workingCaseload.id,
+                [Map.of("locationId", 1,
+                        "locationType", "VLB",
+                        "description", "VLB Room 1",
+                        "userDescription", "VLB Room 1",
+                        "agencyId", "LEI",
+
+                ),
+                 Map.of("locationId", 2,
+                         "locationType", "GYM",
+                         "description", "Gym",
+                         "userDescription", "Gym",
+                         "agencyId", "LEI",
+
+                 )])
+    }
+
+    def setupTestsWithData() {
+        setupTests()
+
+        elite2api.stubGetAppointmentsForAgency(UserAccount.ITAG_USER.workingCaseload.id, [
+                [
+                        id: 1,
+                        offenderNo: 'ABC123',
+                        firstName: 'OFFENDER',
+                        lastName: 'ONE',
+                        date: '2020-01-02',
+                        startTime: '2020-01-02T12:30:00',
+                        appointmentTypeDescription: 'Medical - Other',
+                        appointmentTypeCode: 'MEOT',
+                        locationDescription: 'HEALTH CARE',
+                        locationId: 123,
+                        auditUserId: 'STAFF_1',
+                        agencyId: 'MDI'
+                ],
+                [
+                        id: 2,
+                        offenderNo: 'ABC456',
+                        firstName: 'OFFENDER',
+                        lastName: 'TWO',
+                        date: '2020-01-02',
+                        startTime: '2020-01-02T13:30:00',
+                        endTime: '2020-01-02T14:30:00',
+                        appointmentTypeDescription: 'Gym - Exercise',
+                        appointmentTypeCode: 'GYM',
+                        locationDescription: 'GYM',
+                        locationId: 456,
+                        auditUserId: 'STAFF_2',
+                        agencyId: 'MDI',
+                ],
+                [
+                        id: 3,
+                        offenderNo: 'ABC789',
+                        firstName: 'OFFENDER',
+                        lastName: 'THREE',
+                        date: '2020-01-02',
+                        startTime: '2020-01-02T14:30:00',
+                        endTime: '2020-01-02T15:30:00',
+                        appointmentTypeDescription: 'Video Link booking',
+                        appointmentTypeCode: 'VLB',
+                        locationDescription: 'VCC ROOM',
+                        locationId: 789,
+                        auditUserId: 'STAFF_3',
+                        agencyId: 'MDI',
+                ],
+        ])
+        elite2api.stubUserDetails('STAFF_1', [
+                firstName: "Staff",
+                lastName: "One"
+        ])
+        elite2api.stubUserDetails('STAFF_2', [
+                firstName: "Staff",
+                lastName: "Two"
+        ])
+        elite2api.stubUserDetails('STAFF_3', [
+                firstName: "Staff",
+                lastName: "Three"
+        ])
+        whereaboutsApi.getVideoLinkAppointments([
+                appointments: [
+                        [
+                                id: 1,
+                                bookingId: 1,
+                                appointmentId: 3,
+                                court: 'Wimbledon',
+                                hearingType: 'MAIN'
+                        ]
+                ]
+        ])
+    }
+}
