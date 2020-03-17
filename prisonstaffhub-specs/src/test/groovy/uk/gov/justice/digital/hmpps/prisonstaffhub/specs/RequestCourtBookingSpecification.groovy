1e951ff3c80433b1194101a2b5f33f3ecff33e1a
JamieDraperUK <draper65@googlemail.com>
2020-03-17 10:12:37 +0000

    NN-2495 fix integration test

--- prisonstaffhub-specs/src/test/groovy/uk/gov/justice/digital/hmpps/prisonstaffhub/specs/RequestCourtBookingSpecification.groovy
+++ prisonstaffhub-specs/src/test/groovy/uk/gov/justice/digital/hmpps/prisonstaffhub/specs/RequestCourtBookingSpecification.groovy
@@ -31,6 +31,7 @@ class RequestCourtBookingSpecification extends BrowserReportingSpec {
     def "should request a video link booking"() {
         whereaboutsApi.stubCourtLocations()
         oauthApi.stubGetEmail("ITAG_USER", "itag@local")
+        oauthApi.stubGetUserDetails("ITAG_USER", "staff name")
         elite2api.stubGetAgencies([["agencyId"   : "WWI", "description": "HMP Wandsworth"]])
 
         def tomorrow = LocalDateTime.now()
