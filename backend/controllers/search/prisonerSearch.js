a730522212d4bd375e0a5a3139031aaea8a6bc27
John Robertson <jsrobertson@gmail.com>
2020-03-30 16:17:20 +0100

    NN-2511 Show results on search page

--- backend/controllers/search/prisonerSearch.js
+++ backend/controllers/search/prisonerSearch.js
@@ -1,34 +1,23 @@
 const moment = require('moment')
-const querystring = require('querystring')
 const { serviceUnavailableMessage } = require('../../common-messages')
 const { formatName } = require('../../utils')
 const config = require('../../config')
 const prisonerSearchValidation = require('./prisonerSearchValidation')
 
 const prisonerSearchFactory = (oauthApi, elite2Api, logError) => {
-  const renderError = (req, res, error) => {
-    if (error) logError(req.originalUrl, error, serviceUnavailableMessage)
-
-    return res.render('courtServiceError.njk', { url: '/', homeUrl: '/videolink' })
-  }
-
-  const renderTemplate = async (req, res, pageData) => {
+  const index = async (req, res) => {
     try {
-      const [userRoles, agencies] = await Promise.all([
-        oauthApi.userRoles(res.locals),
-        elite2Api.getAgencies(res.locals),
-      ])
+      const userRoles = await oauthApi.userRoles(res.locals)
       const hasSearchAccess = userRoles.find(role => role.roleCode === 'VIDEO_LINK_COURT_USER')
-      const agencyOptions = agencies
-        .map(agency => ({ value: agency.agencyId, text: agency.description }))
-        .sort((a, b) => a.text.localeCompare(b.text))
-      const hasSearched = Object.keys(req.query).length
-      let searchResults = []
 
       if (hasSearchAccess) {
+        const agencies = await elite2Api.getAgencies(res.locals)
+        let searchResults = []
+        const hasSearched = Object.keys(req.query).length
+        const errors = hasSearched && prisonerSearchValidation(req.query)
         const { firstName, lastName, prisonNumber, dobDay, dobMonth, dobYear, prison } = req.query
 
-        if (hasSearched) {
+        if (hasSearched && !errors.length) {
           const dateOfBirth = moment({
             day: dobDay,
             month: Number.isNaN(dobMonth) ? dobMonth : dobMonth - 1,
@@ -47,61 +36,45 @@ const prisonerSearchFactory = (oauthApi, elite2Api, logError) => {
           })
         }
 
-        const results = searchResults
-          .filter(result => (prison ? prison === result.latestLocationId : result))
-          .map(result => {
-            const { offenderNo, latestLocationId, pncNumber } = result
-            const name = formatName(result.firstName, result.lastName)
-
-            return {
-              name,
-              offenderNo,
-              dob: result.dateOfBirth ? moment(result.dateOfBirth).format('D MMMM YYYY') : undefined,
-              prison: result.latestLocation,
-              prisonId: latestLocationId,
-              pncNumber: pncNumber || '--',
-              addAppointmentHTML: config.app.videoLinkEnabledFor.includes(latestLocationId)
-                ? `<a href="/${latestLocationId}/offenders/${offenderNo}/add-court-appointment" class="govuk-link" data-qa="book-vlb-link">Book video link<span class="visually-hidden"> for ${name}, prison number ${offenderNo}</span></a>`
-                : '',
-            }
-          })
-
-        const hasOtherSearchDetails = prisonNumber || dobDay || dobMonth || dobYear || prison
-
         return res.render('prisonerSearch.njk', {
-          ...pageData,
-          agencyOptions,
-          results,
-          hasSearched,
-          homeUrl: '/videolink',
+          agencyOptions: agencies
+            .map(agency => ({ value: agency.agencyId, text: agency.description }))
+            .sort((a, b) => a.text.localeCompare(b.text)),
+          errors,
           formValues: hasSearched ? req.query : req.body,
-          hasOtherSearchDetails,
+          homeUrl: '/videolink',
+          results: searchResults
+            .filter(result => (prison ? prison === result.latestLocationId : result))
+            .map(result => {
+              const { offenderNo, latestLocationId, pncNumber } = result
+              const name = formatName(result.firstName, result.lastName)
+
+              return {
+                name,
+                offenderNo,
+                dob: result.dateOfBirth ? moment(result.dateOfBirth).format('D MMMM YYYY') : undefined,
+                prison: result.latestLocation,
+                prisonId: latestLocationId,
+                pncNumber: pncNumber || '--',
+                addAppointmentHTML: config.app.videoLinkEnabledFor.includes(latestLocationId)
+                  ? `<a href="/${latestLocationId}/offenders/${offenderNo}/add-court-appointment" class="govuk-link" data-qa="book-vlb-link">Book video link<span class="visually-hidden"> for ${name}, prison number ${offenderNo}</span></a>`
+                  : '',
+              }
+            }),
+          hasSearched,
+          hasOtherSearchDetails: prisonNumber || dobDay || dobMonth || dobYear || prison,
         })
       }
 
       return res.redirect('back')
     } catch (error) {
-      return renderError(req, res, error)
+      if (error) logError(req.originalUrl, error, serviceUnavailableMessage)
+
+      return res.render('courtServiceError.njk', { url: '/', homeUrl: '/videolink' })
     }
   }
 
-  const index = async (req, res) => renderTemplate(req, res)
-
-  const post = async (req, res) => {
-    const errors = prisonerSearchValidation(req.body)
-
-    if (errors.length > 0) {
-      return renderTemplate(req, res, {
-        errors,
-      })
-    }
-
-    const searchQuery = querystring.stringify(req.body)
-
-    return res.redirect(`/prisoner-search?${searchQuery}`)
-  }
-
-  return { index, post }
+  return { index }
 }
 
 module.exports = {
--- backend/routes/prisonerSearchRouter.js
+++ backend/routes/prisonerSearchRouter.js
@@ -4,10 +4,9 @@ const { prisonerSearchFactory } = require('../controllers/search/prisonerSearch'
 const router = express.Router({ mergeParams: true })
 
 const controller = ({ oauthApi, elite2Api, logError }) => {
-  const { index, post } = prisonerSearchFactory(oauthApi, elite2Api, logError)
+  const { index } = prisonerSearchFactory(oauthApi, elite2Api, logError)
 
   router.get('/', index)
-  router.post('/', post)
 
   return router
 }
