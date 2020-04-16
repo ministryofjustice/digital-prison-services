f98164fed2a4204bffb0a70a02d3f10383467dc0
John Robertson <jsrobertson@users.noreply.github.com>
2020-04-16 10:23:59 +0100

    NN-2457 Add Offence, Money and Personal sections to new quick look (#903)
    * NN-2457 Add data to offence and money sections on quick look
    
    * NN-2457 Add personal details section

--- backend/controllers/prisonerProfile/prisonerQuickLook.js
+++ backend/controllers/prisonerProfile/prisonerQuickLook.js
@@ -1,7 +1,9 @@
+const moment = require('moment')
 const { serviceUnavailableMessage } = require('../../common-messages')
 const {
   app: { notmEndpointUrl: dpsUrl },
 } = require('../../config')
+const { formatTimestampToDate, formatCurrency } = require('../../utils')
 
 module.exports = ({ prisonerProfileService, elite2Api, logError }) => async (req, res) => {
   try {
@@ -9,21 +11,45 @@ module.exports = ({ prisonerProfileService, elite2Api, logError }) => async (req
     const details = await elite2Api.getDetails(res.locals, offenderNo)
     const { bookingId } = details
 
-    const [prisonerProfileData, offenceData] = await Promise.all([
+    const [prisonerProfileData, offenceData, balanceData, prisonerData, sentenceData] = await Promise.all([
       prisonerProfileService.getPrisonerProfileData(res.locals, offenderNo),
       elite2Api.getMainOffence(res.locals, bookingId),
+      elite2Api.getPrisonerBalances(res.locals, bookingId),
+      elite2Api.getPrisonerDetails(res.locals, offenderNo),
+      elite2Api.getPrisonerSentenceDetails(res.locals, offenderNo),
     ])
 
+    const prisoner = Boolean(prisonerData.length) && prisonerData[0]
+
+    const { sentenceDetail } = sentenceData
     const offenceDetails = [
+      { label: 'Main offence(s)', value: Boolean(offenceData.length) && offenceData[0].offenceDescription },
+      { label: 'Imprisonment status', value: prisoner && prisoner.imprisonmentStatusDesc },
       {
-        key: { text: 'Main offence(s)' },
-        value: { text: Boolean(offenceData.length) && offenceData[0].offenceDescription },
+        label: 'Release date',
+        value: sentenceDetail && sentenceDetail.releaseDate && formatTimestampToDate(sentenceDetail.releaseDate),
       },
+    ]
+
+    const { currency } = balanceData
+    const balanceDetails = [
+      { label: 'Spends', value: formatCurrency(balanceData.spends, currency) },
+      { label: 'Private', value: formatCurrency(balanceData.cash, currency) },
+      { label: 'Savings', value: formatCurrency(balanceData.savings, currency) },
+    ]
+
+    const personalDetails = [
+      { label: 'Age', value: prisoner.dateOfBirth && moment().diff(moment(prisoner.dateOfBirth), 'years') },
+      { label: 'Nationality', value: prisoner.nationalities },
+      { label: 'PNC number', value: prisoner.pncNumber },
+      { label: 'CRO number', value: prisoner.croNumber },
     ]
 
     return res.render('prisonerProfile/prisonerQuickLook.njk', {
       prisonerProfileData,
       offenceDetails,
+      balanceDetails,
+      personalDetails,
     })
   } catch (error) {
     logError(req.originalUrl, error, serviceUnavailableMessage)
