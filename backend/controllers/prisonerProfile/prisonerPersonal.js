35d7e31e4b68f94e31718eb74bf3b98cb2dbdd2e
John Robertson <jsrobertson@gmail.com>
2020-04-28 11:53:24 +0100

    NN-2581 Add identifiers to personal page

--- backend/controllers/prisonerProfile/prisonerPersonal.js
+++ backend/controllers/prisonerProfile/prisonerPersonal.js
@@ -1,9 +1,41 @@
-module.exports = ({ prisonerProfileService }) => async (req, res) => {
-  const { offenderNo } = req.params
+const { serviceUnavailableMessage } = require('../../common-messages')
+const logErrorAndContinue = require('../../shared/logErrorAndContinue')
+const {
+  app: { notmEndpointUrl: dpsUrl },
+} = require('../../config')
 
-  const prisonerProfileData = await prisonerProfileService.getPrisonerProfileData(res.locals, offenderNo)
+const getIdentifierValue = (type, identifiers) => {
+  const identifier = identifiers && identifiers.length > 0 ? identifiers.find(id => id.type === type) : identifiers
+
+  return identifier && identifier.value
+}
+
+module.exports = ({ prisonerProfileService, elite2Api, logError }) => async (req, res) => {
+  const { offenderNo } = req.params
+  const details = await elite2Api
+    .getDetails(res.locals, offenderNo)
+    .then(data => data)
+    .catch(error => {
+      logError(req.originalUrl, error, serviceUnavailableMessage)
+      return res.render('error.njk', { url: dpsUrl })
+    })
+  const { bookingId } = details || {}
+
+  const [prisonerProfileData, identifierData] = await Promise.all(
+    [
+      prisonerProfileService.getPrisonerProfileData(res.locals, offenderNo),
+      elite2Api.getIdentifiers(res.locals, bookingId),
+    ].map(apiCall => logErrorAndContinue(apiCall))
+  )
 
   return res.render('prisonerProfile/prisonerPersonal.njk', {
     prisonerProfileData,
+    identifiers: [
+      { label: 'PNC number', value: getIdentifierValue('PNC', identifierData) },
+      { label: 'CRO number', value: getIdentifierValue('CRO', identifierData) },
+      { label: 'National insurance number', value: getIdentifierValue('NINO', identifierData) },
+      { label: 'Home office reference number', value: getIdentifierValue('HOREF', identifierData) },
+      { label: 'Driving licence number', value: getIdentifierValue('DL', identifierData) },
+    ],
   })
 }
--- backend/routes/prisonerProfileRouter.js
+++ backend/routes/prisonerProfileRouter.js
@@ -11,7 +11,7 @@ const controller = ({ elite2Api, keyworkerApi, oauthApi, logError }) => {
 
   router.get('/', prisonerQuickLook({ prisonerProfileService, elite2Api, logError }))
   router.get('/image', prisonerFullImage({ elite2Api, logError }))
-  router.get('/personal', prisonerPersonal({ prisonerProfileService }))
+  router.get('/personal', prisonerPersonal({ prisonerProfileService, elite2Api, logError }))
 
   return router
 }
