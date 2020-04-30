6eae2950a9fb6a607944b469c51abbb75e08da1f
JamieDraperUK <draper65@googlemail.com>
2020-04-30 13:37:34 +0100

    NN-2473 fix test

--- backend/routes/attendanceChangesRouter.js
+++ backend/routes/attendanceChangesRouter.js
@@ -56,7 +56,7 @@ module.exports = ({ elite2Api, whereaboutsApi, oauthApi, logError }) => async (r
     const eventIds = [...new Set(changes.map(change => change.eventId))]
     const activities = await elite2Api.getScheduledActivities(res.locals, { agencyId: 'MDI', eventIds })
 
-    const userNames = [...new Set(changes.map(change => oauthApi.userDetails(res.locals, change.changedBy)))]
+    const userNames = [...new Set(changes.map(change => change.changedBy))]
     const userDetails = await Promise.all(userNames.map(username => oauthApi.userDetails(res.locals, username)))
 
     const activitiesMap = toActivitiesMap(activities)
