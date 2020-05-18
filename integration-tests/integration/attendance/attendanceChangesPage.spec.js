b13d20c1d4d9b49f783ba734df5b8a0f58ca356e
Nevelina Aleksandrova <nevelina@diginaga.online>
2020-05-18 11:19:13 +0100

    NN-2629: Pass agency id as part of url, refactor integration test into cypress

---
+++ integration-tests/integration/attendance/attendanceChangesPage.spec.js
@@ -0,0 +1,41 @@
+const AttendanceChangesPage = require('../../pages/attendance/attendanceChangesPage')
+
+context('A user can view attendance changes', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubLogin', { username: 'ITAG_USER', caseload: 'WWI' })
+    cy.login()
+    cy.task('stubAttendanceChanges', [
+      {
+        eventId: 1,
+        changedBy: 'ITAG_USER',
+        changedOn: '2010-10-10T20:00',
+        changedFrom: 'Attended',
+        changedTo: 'Refused',
+      },
+    ])
+    cy.task('stubScheduledActivities', [
+      { eventId: 1, comment: 'Houseblock 1', firstName: 'bob', lastName: 'sut', offenderNo: 'A123456' },
+    ])
+
+    const fromDateTime = '2010-10-10T10:00'
+    const toDateTime = '2010-10-10T17:00'
+    const subHeading = 'test'
+    const agencyId = 'WWI'
+    cy.visit(
+      `/attendance-changes?agencyId=${agencyId}&fromDateTime=${fromDateTime}&toDateTime=${toDateTime}&subHeading=${subHeading}`
+    )
+  })
+
+  it('A user can view attendance changes for specific date and agency', () => {
+    const attendanceChangesPage = AttendanceChangesPage.verifyOnPage()
+    const tableDataRow = attendanceChangesPage.getChangesRows(0)
+    tableDataRow.name().contains('Sut, Bob')
+    tableDataRow.prisonNo().contains('A123456')
+    tableDataRow.activity().contains('Houseblock 1')
+    tableDataRow.changedFrom().contains('Attended')
+    tableDataRow.changedTo().contains('Refused')
+    tableDataRow.dateAndTime().contains('10 October 2010 - 20:00')
+    tableDataRow.changedBy().contains('ITAG_USER name')
+  })
+})
--- integration-tests/plugins/index.js
+++ integration-tests/plugins/index.js
@@ -1,5 +1,6 @@
 const auth = require('../mockApis/auth')
 const elite2api = require('../mockApis/elite2')
+const whereabouts = require('../mockApis/whereabouts')
 const { resetStubs } = require('../mockApis/wiremock')
 
 module.exports = on => {
@@ -7,7 +8,12 @@ module.exports = on => {
     reset: () => Promise.all([resetStubs()]),
 
     getLoginUrl: auth.getLoginUrl,
-    stubLogin: () => Promise.all([auth.stubLogin({}), elite2api.stubUser(), elite2api.stubUserCaseloads()]),
+    stubLogin: ({ username, caseload }) =>
+      Promise.all([auth.stubLogin(username, caseload), elite2api.stubUserMe(), elite2api.stubUserCaseloads()]),
     stubLoginCourt: () => Promise.all([auth.stubLoginCourt({}), elite2api.stubUser(), elite2api.stubUserCaseloads()]),
+
+    stubScheduledActivities: response => Promise.all([elite2api.stubUserScheduledActivities(response)]),
+
+    stubAttendanceChanges: response => Promise.all([whereabouts.stubAttendanceChanges(response)]),
   })
 }
