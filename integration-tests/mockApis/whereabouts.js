e61eea80edd07df8c7153e93becbd171e1618572
JamieDraperUK <draper65@googlemail.com>
2020-05-22 14:34:14 +0100

    NN-2629 revert test api changes

--- integration-tests/mockApis/whereabouts.js
+++ integration-tests/mockApis/whereabouts.js
@@ -1,30 +1,21 @@
-const { getFor } = require('./wiremock')
+const { stubFor } = require('./wiremock')
 
 module.exports = {
-  stubAttendanceChanges: changes =>
-    getFor({
-      urlPattern: '/attendances/changes\\?fromDateTime=.+?&toDateTime=.+?',
-      body: changes,
-    }),
-  stubAttendanceStats: ({ agencyId, stats }) =>
-    getFor({
-      urlPath: `/attendance-statistics/${agencyId}/over-date-range`,
-      body: stats,
-    }),
-  stubAbsenceReasons: () =>
-    getFor({
-      urlPath: '/attendance/absence-reasons',
-      body: {
-        triggersIEPWarning: ['UnacceptableAbsence', 'RefusedIncentiveLevelWarning'],
-        paidReasons: ['ApprovedCourse', 'AcceptableAbsence', 'NotRequired'],
-        unpaidReasons: [
-          'SessionCancelled',
-          'RestInCellOrSick',
-          'RestDay',
-          'UnacceptableAbsence',
-          'Refused',
-          'RefusedIncentiveLevelWarning',
-        ],
+  stubAttendanceChanges: changes => {
+    return stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/attendances/changes\\?fromDateTime=.+?&toDateTime=.+?',
       },
-    }),
+      response: {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+        },
+        jsonBody: {
+          changes,
+        },
+      },
+    })
+  },
 }
