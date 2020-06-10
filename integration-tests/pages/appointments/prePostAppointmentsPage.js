908e6fbd3d5549743eecc1de690279ff76fdb458
Nevelina Aleksandrova <nevelina@diginaga.online>
2020-06-10 16:06:26 +0100

    NN-2646: More (failing) cypress tests

--- integration-tests/pages/appointments/prePostAppointmentsPage.js
+++ integration-tests/pages/appointments/prePostAppointmentsPage.js
@@ -2,8 +2,18 @@ const page = require('../page')
 
 const prePostAppointmentsPage = () =>
   page('Video link booking details', {
-    form: () => cy.get('form'),
-    submitButton: () => cy.get('button[type="submit"]'),
+    form: () => ({
+      preAppointmentYes: () => cy.get('#preAppointment'),
+      preAppointmentLocation: () => cy.get('#preAppointmentLocation'),
+      preAppointmentDuration: () => cy.get('#preAppointmentDuration'),
+      preAppointmentNo: () => cy.get('#preAppointment-2'),
+      postAppointmentYes: () => cy.get('#postAppointment'),
+      postAppointmentLocation: () => cy.get('#postAppointmentLocation'),
+      postAppointmentDuration: () => cy.get('#postAppointmentDuration'),
+      postAppointmentNo: () => cy.get('#postAppointment-2'),
+      court: () => cy.get('#court'),
+      submitButton: () => cy.get('button[type="submit"]'),
+    }),
     errorSummary: () => cy.get('.govuk-error-summary'),
   })
 
