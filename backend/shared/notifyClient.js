f11e6fc914b7f3e44cbbf08b5ee2ab8c92d64dfc
Nevelina Aleksandrova <nevelina@diginaga.online>
2020-06-11 14:24:20 +0100

    NN-2646: Fix sendEmail stub

--- backend/shared/notifyClient.js
+++ backend/shared/notifyClient.js
@@ -1,7 +1,9 @@
 const { NotifyClient } = require('notifications-node-client')
 const config = require('../config')
 
-const notifyClient = config.notifications.enabled ? new NotifyClient(config.notifications.notifyKey) : { sendEmail: {} }
+const notifyClient = config.notifications.enabled
+  ? new NotifyClient(config.notifications.notifyKey)
+  : { sendEmail() {} }
 
 module.exports = {
   notifyClient,
