8446f9a6abb661a8ba28d9b71223e4a6d601d3c7
John Robertson <jsrobertson@gmail.com>
2020-06-11 15:46:42 +0100

    NN-2710 hide contact phone and email labels if there are no values

--- backend/controllers/prisonerProfile/personalViewModels/activeContactsViewModel.js
+++ backend/controllers/prisonerProfile/personalViewModels/activeContactsViewModel.js
@@ -6,31 +6,41 @@ module.exports = ({ personal, professional }) => {
   const contactHasActiveAddress = contact =>
     contact.addresses.find(address => !address.endDate || moment(address.endDate).isAfter())
 
+  const hasLength = array => array && array.length > 0
+
   return {
     personal:
       personal &&
-      personal.map(contact => ({
-        name: formatName(contact.firstName, contact.lastName),
-        emergencyContact: contact.emergencyContact,
-        details: [
-          { label: 'Relationship', value: contact.relationshipDescription },
-          { label: 'Phone number', value: getPhone(contact.phones) },
-          { label: 'Email', value: contact.emails && contact.emails.map(email => email.email).join(', ') },
-          ...getAddress({ address: contact.addresses.find(address => address.primary) }),
-        ],
-      })),
+      personal.map(contact => {
+        const { phones, emails } = contact
+
+        return {
+          name: formatName(contact.firstName, contact.lastName),
+          emergencyContact: contact.emergencyContact,
+          details: [
+            { label: 'Relationship', value: contact.relationshipDescription },
+            ...(hasLength(phones) ? [{ label: 'Phone number', value: getPhone(phones) }] : []),
+            ...(hasLength(emails) ? [{ label: 'Email', value: emails.map(email => email.email).join(', ') }] : []),
+            ...getAddress({ address: contact.addresses.find(address => address.primary) }),
+          ],
+        }
+      }),
     professional:
       professional &&
-      professional.filter(contactHasActiveAddress).map(contact => ({
-        name: formatName(contact.firstName, contact.lastName),
-        details: [
-          { label: 'Relationship', value: contact.relationshipDescription },
-          { label: 'Phone number', value: getPhone(contact.phones) },
-          { label: 'Email', value: contact.emails && contact.emails.map(email => email.email).join(', ') },
-          ...getAddress({
-            address: contactHasActiveAddress(contact),
-          }),
-        ],
-      })),
+      professional.filter(contactHasActiveAddress).map(contact => {
+        const { phones, emails } = contact
+
+        return {
+          name: formatName(contact.firstName, contact.lastName),
+          details: [
+            { label: 'Relationship', value: contact.relationshipDescription },
+            ...(hasLength(phones) ? [{ label: 'Phone number', value: getPhone(phones) }] : []),
+            ...(hasLength(emails) ? [{ label: 'Email', value: emails.map(email => email.email).join(', ') }] : []),
+            ...getAddress({
+              address: contactHasActiveAddress(contact),
+            }),
+          ],
+        }
+      }),
   }
 }
