4e8a60a7056e499306e7a0b990fb564aea82a106
John Robertson <jsrobertson@gmail.com>
2020-06-17 08:45:00 +0100

    NN-2630 Move list view into own partial

--- backend/controllers/search/prisonerSearch.js
+++ backend/controllers/search/prisonerSearch.js
@@ -5,16 +5,14 @@ const { putLastNameFirst } = require('../../utils')
 module.exports = ({ paginationService, elite2Api, logError }) => async (req, res) => {
   try {
     const fullUrl = new URL(`${req.protocol}://${req.get('host')}${req.originalUrl}`)
-    const { location, keywords, alerts, pageOffsetOption, pageLimit = 50 } = req.query
+    const { location, keywords, alerts, pageOffsetOption } = req.query
 
     const pageOffset = parseInt(pageOffsetOption, 10) || 0
+    const pageLimit = 50
 
     const hasSearched = Boolean(Object.keys(req.query).length)
 
-    const context = {
-      ...res.locals,
-      requestHeaders: { 'page-offset': pageOffset, 'page-limit': pageLimit },
-    }
+    const context = { ...res.locals, requestHeaders: { 'page-offset': pageOffset, 'page-limit': pageLimit } }
 
     const [locations, prisoners] = await Promise.all([
       elite2Api.userLocations(res.locals),
@@ -26,8 +24,6 @@ module.exports = ({ paginationService, elite2Api, logError }) => async (req, res
         returnCategory: 'true',
       }),
     ])
-
-    const totalPrisoners = context.responseHeaders['total-records']
 
     const locationOptions = locations && locations.map(loc => ({ value: loc.locationPrefix, text: loc.description }))
 
@@ -47,11 +43,15 @@ module.exports = ({ paginationService, elite2Api, logError }) => async (req, res
         text: alertFlag.label,
         checked: alertFlag.alertCodes.some(alert => alerts && alerts.includes(alert)),
       })),
-      errors: [],
       formValues: req.query,
       hasSearched,
       locationOptions,
-      pagination: paginationService.getPagination(totalPrisoners, pageOffset, pageLimit, fullUrl),
+      pagination: paginationService.getPagination(
+        context.responseHeaders['total-records'],
+        pageOffset,
+        pageLimit,
+        fullUrl
+      ),
       results,
     })
   } catch (error) {
