a787c3c9c23b7c530da7fc87a8900b07aa25b6e5
Nevelina Aleksandrova <nevelina@diginaga.online>
2020-07-02 09:39:34 +0100

    NN-2745: Attempt to make healthcheck more robust

--- integration-tests/integration/health.spec.js
+++ integration-tests/integration/health.spec.js
@@ -1,4 +1,24 @@
 context('Health page reports health correctly', () => {
+  it('Reports correctly when some down', () => {
+    cy.task('reset')
+    cy.task('stubHealth', { authStatus: 500 })
+    cy.request({ url: '/health', method: 'GET', failOnStatusCode: false }).then(response => {
+      expect(response.body.name).to.equal('prisonstaffhub')
+      // eslint-disable-next-line no-unused-expressions
+      expect(response.body.version).to.not.be.empty
+      expect(response.body.api).to.deep.equal({
+        allocationManager: 'UP',
+        auth: { timeout: 1000, code: 'ECONNABORTED', errno: 'ETIMEDOUT', retries: 2 },
+        casenotes: 'UP',
+        community: 'UP',
+        elite2: 'UP',
+        keyworker: 'UP',
+        tokenverification: 'UP',
+        whereabouts: 'UP',
+      })
+    })
+  })
+
   it('Reports correctly when all are up', () => {
     cy.task('reset')
     cy.task('stubHealth', {})
@@ -10,26 +30,6 @@ context('Health page reports health correctly', () => {
       expect(response.body.api).to.deep.equal({
         allocationManager: 'UP',
         auth: 'UP',
-        casenotes: 'UP',
-        community: 'UP',
-        elite2: 'UP',
-        keyworker: 'UP',
-        tokenverification: 'UP',
-        whereabouts: 'UP',
-      })
-    })
-  })
-
-  it('Reports correctly when some down', () => {
-    cy.task('reset')
-    cy.task('stubHealth', { authStatus: 500 })
-    cy.request({ url: '/health', method: 'GET', failOnStatusCode: false }).then(response => {
-      expect(response.body.name).to.equal('prisonstaffhub')
-      // eslint-disable-next-line no-unused-expressions
-      expect(response.body.version).to.not.be.empty
-      expect(response.body.api).to.deep.equal({
-        allocationManager: 'UP',
-        auth: { timeout: 1000, code: 'ECONNABORTED', errno: 'ETIMEDOUT', retries: 2 },
         casenotes: 'UP',
         community: 'UP',
         elite2: 'UP',
