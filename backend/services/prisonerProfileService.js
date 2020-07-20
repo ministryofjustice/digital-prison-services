1347100974b6a5c50c20948a6cde04c8d0b895e8
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2020-07-20 10:33:34 +0100

    DCS-575 Fix report use of force url (#1120)

--- backend/services/prisonerProfileService.js
+++ backend/services/prisonerProfileService.js
@@ -123,7 +123,7 @@ module.exports = ({ elite2Api, keyworkerApi, oauthApi, dataComplianceApi, pathfi
       offenderRecordRetained: offenderRetentionRecord && hasLength(offenderRetentionRecord.retentionReasons),
       showAddKeyworkerSession: staffRoles && staffRoles.some(role => role.role === 'KW'),
       showReportUseOfForce: useOfForceEnabledPrisons.includes(currentUser.activeCaseLoadId),
-      useOfForceUrl,
+      useOfForceUrl: `${useOfForceUrl}/report/${bookingId}/report-use-of-force`,
       userCanEdit: (canViewInactivePrisoner && ['OUT', 'TRN'].includes(agencyId)) || offenderInCaseload,
       language,
       interpreterRequired,
