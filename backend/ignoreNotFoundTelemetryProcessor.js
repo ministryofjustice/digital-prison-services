e8b7d97fcf0be4361e84d1bc8ec94fb8961d9b6e
JamieDraperUK <draper65@googlemail.com>
2020-07-22 12:46:39 +0100

    NN-2811 ignore 404 appinsights

--- backend/ignoreNotFoundTelemetryProcessor.js
+++ backend/ignoreNotFoundTelemetryProcessor.js
@@ -1,7 +1,6 @@
 module.exports = function ignoreNotFoundErrors(envelope) {
   if (envelope && envelope.data && envelope.data.baseData) {
     if (envelope.data.baseType === 'RemoteDependencyData' && envelope.data.baseData.resultCode === '404') return false
-    if (envelope.data.baseType === 'RequestData' && envelope.data.baseData.responseCode === '404') return false
   }
 
   return true
--- backend/ignoreNotFoundTelemetryProcessor.test.js
+++ backend/ignoreNotFoundTelemetryProcessor.test.js
@@ -14,25 +14,10 @@ describe('Ignore http NotFound errors telemetry processor', () => {
     ).toBe(false)
   })
 
-  it('should return false when a base type of RequestData and responseCode equals 404', () => {
-    expect(
-      ignoreNotFoundErrors({
-        data: {
-          baseType: 'RequestData',
-          baseData: {
-            responseCode: '404',
-          },
-        },
-      })
-    ).toBe(false)
-  })
-
   it('should handle crappy data', () => {
     expect(ignoreNotFoundErrors()).toBe(true)
     expect(ignoreNotFoundErrors({ data: {} })).toBe(true)
-    expect(ignoreNotFoundErrors({ data: { baseType: 'RequestData' } })).toBe(true)
     expect(ignoreNotFoundErrors({ data: { baseType: 'RemoteDependencyData' } })).toBe(true)
-    expect(ignoreNotFoundErrors({ data: { baseType: 'RequestData', baseData: {} } })).toBe(true)
     expect(ignoreNotFoundErrors({ data: { baseType: 'RemoteDependencyData', baseData: {} } })).toBe(true)
   })
 })
