dbedf0570c2e759d174ac6381470346f224a1709
John Robertson <jsrobertson@gmail.com>
2020-07-27 13:49:24 +0100

    NN-2711 Add integration tests

--- integration-tests/mockApis/elite2.js
+++ integration-tests/mockApis/elite2.js
@@ -1220,4 +1220,32 @@ module.exports = {
         jsonBody: sentenceTerms || [],
       },
     }),
+  stubScheduleForThisWeek: (events, status = 200) =>
+    stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/api/bookings/[0-9]+?/appointments/thisWeek',
+      },
+      response: {
+        status,
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+        },
+        jsonBody: events || [],
+      },
+    }),
+  stubScheduleForNextWeek: (events, status = 200) =>
+    stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/api/bookings/[0-9]+?/appointments/nextWeek',
+      },
+      response: {
+        status,
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+        },
+        jsonBody: events || [],
+      },
+    }),
 }
