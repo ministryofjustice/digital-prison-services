8afb222b43862bc1cf371412e2748cccf851b5a4
Nevelina Aleksandrova <nevelina@diginaga.online>
2020-07-27 14:01:17 +0100

    NN-2708: Add cypress test

---
+++ integration-tests/integration/caseNotes/createCaseNote.spec.js
@@ -0,0 +1,71 @@
+const offenderBasicDetails = require('../../mockApis/responses/offenderBasicDetails.json')
+const offenderFullDetails = require('../../mockApis/responses/offenderFullDetails.json')
+const CreateCaseNotePage = require('../../pages/caseNotes/createCaseNotePage')
+const PrisonerCaseNotePage = require('../../pages/prisonerProfile/caseNotePage')
+
+context('A user can add a case note', () => {
+  before(() => {
+    cy.clearCookies()
+    cy.task('reset')
+    cy.task('stubLogin', { username: 'ITAG_USER', caseload: 'MDI' })
+    cy.login()
+  })
+  beforeEach(() => {
+    Cypress.Cookies.preserveOnce('hmpps-session-dev')
+    cy.task('resetAndStubTokenVerification')
+    cy.task('stubOffenderBasicDetails', offenderBasicDetails)
+    const offenderNo = 'A12345'
+    cy.task('stubCaseNoteTypesForUser')
+    cy.task('stubCreateCaseNote')
+
+    cy.visit(`/offenders/${offenderNo}/add-case-note`)
+  })
+
+  it('A user can successfully add a case note', () => {
+    cy.task('stubClientCredentialsRequest')
+    cy.task('stubPrisonerProfileHeaderData', {
+      offenderBasicDetails,
+      offenderFullDetails,
+      iepSummary: {},
+      caseNoteSummary: {},
+    })
+    cy.task('stubCaseNoteTypes')
+    cy.task('stubCaseNotes', {
+      totalElements: 1,
+      content: [],
+    })
+    const createCaseNotePage = CreateCaseNotePage.verifyOnPage()
+    const form = createCaseNotePage.form()
+    form.type().select('OBSERVE')
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(50)
+    cy.get('#sub-type').select('test')
+    form.text().type('Test comment')
+    form.date().type('20/07/2020')
+    createCaseNotePage.activeDate().click()
+    form.hours().type('10')
+    form.minutes().type('11')
+    form.submitButton().click()
+    PrisonerCaseNotePage.verifyOnPage('Smith, John')
+  })
+
+  it('Should show correct error messages', () => {
+    const createCaseNotePage = CreateCaseNotePage.verifyOnPage()
+    const form = createCaseNotePage.form()
+    form.submitButton().click()
+
+    CreateCaseNotePage.verifyOnPage()
+    createCaseNotePage.errorSummaryTitle().contains('There is a problem')
+    createCaseNotePage
+      .errorSummaryList()
+      .find('li')
+      .then($errors => {
+        expect($errors.get(0).innerText).to.contain('Select the case note type')
+        expect($errors.get(1).innerText).to.contain('Select the case note sub-type')
+        expect($errors.get(2).innerText).to.contain('Enter what happened')
+        expect($errors.get(3).innerText).to.contain('Select the date when this happened')
+        expect($errors.get(4).innerText).to.contain('Enter an hour which is 23 or less')
+        expect($errors.get(5).innerText).to.contain('Enter the minutes using 59 or less')
+      })
+  })
+})
