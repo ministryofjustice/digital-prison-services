6ff167d0f684311d1bffd48d43c19c319be7ef35
petergphillips <peter@greenthistle.co.uk>
2020-07-28 18:19:30 +0100

    DT-1042: ✅ Add in cypress int tests for new global search (#1155)

--- integration-tests/mockApis/offenderSearch.js
+++ integration-tests/mockApis/offenderSearch.js
@@ -1,8 +1,9 @@
-const { stubFor } = require('./wiremock')
+const { stubFor, postFor, getMatchingRequests } = require('./wiremock')
+const globalSearchResponse = require('./responses/globalSearchResponse.json')
 
 module.exports = {
-  stubHealth: (status = 200) => {
-    return stubFor({
+  stubHealth: (status = 200) =>
+    stubFor({
       request: {
         method: 'GET',
         url: '/offenderSearch/health/ping',
@@ -13,6 +14,60 @@ module.exports = {
           'Content-Type': 'application/json;charset=UTF-8',
         },
       },
-    })
+    }),
+  stubGlobalSearch: () =>
+    postFor({
+      urlPattern: '/offenderSearch/global-search\\?size=20',
+      body: {
+        totalElements: 2,
+        pageable: { pageSize: 20, offset: 0 },
+        content: [
+          {
+            prisonerNumber: 'A1234AC',
+            firstName: 'FRED',
+            lastName: 'QUIMBY',
+            prisonName: 'Leeds HMP',
+            dateOfBirth: '1977-10-15',
+            prisonId: 'LEI',
+          },
+          {
+            prisonerNumber: 'A1234AA',
+            firstName: 'ARTHUR',
+            lastName: 'QUIMBY',
+            prisonName: 'Moorland HMP',
+            dateOfBirth: '1976-09-15',
+            prisonId: 'MDI',
+          },
+        ],
+      },
+    }),
+  verifyGlobalSearch: () =>
+    getMatchingRequests({
+      method: 'POST',
+      urlPath: '/offenderSearch/global-search',
+    }).then(data => data.body.requests),
+
+  stubGlobalSearchMultiplePages: () => {
+    const resultsPerPage = 20
+    const firstPage = globalSearchResponse.slice(0, resultsPerPage)
+    const nextPage = globalSearchResponse.slice(resultsPerPage)
+    return Promise.all([
+      postFor({
+        urlPattern: `/offenderSearch/global-search\\?size=${resultsPerPage}`,
+        body: {
+          totalElements: globalSearchResponse.length,
+          pageable: { pageSize: resultsPerPage, offset: 0 },
+          content: firstPage,
+        },
+      }),
+      postFor({
+        urlPattern: `/offenderSearch/global-search\\?page=1&size=${resultsPerPage}`,
+        body: {
+          totalElements: globalSearchResponse.length,
+          pageable: { pageSize: resultsPerPage, offset: resultsPerPage },
+          content: nextPage,
+        },
+      }),
+    ])
   },
 }
--- integration-tests/support/commands.js
+++ integration-tests/support/commands.js
@@ -1,6 +1,6 @@
 import 'cypress-file-upload'
 
-Cypress.Commands.add('login', () => {
-  cy.request('/')
+Cypress.Commands.add('login', (url = '/') => {
+  cy.request(url)
   cy.task('getLoginUrl').then(cy.visit)
 })
