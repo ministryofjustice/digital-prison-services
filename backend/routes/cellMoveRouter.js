87b7f1a3c5768cc463d1305a9b832d5ddc890abe
Nevelina Aleksandrova <nevelina.aleksandrova@digital.justice.gov.uk>
2020-09-10 10:24:15 +0100

    NN-2716: [WIP] Add form to and form controller

--- backend/routes/cellMoveRouter.js
+++ backend/routes/cellMoveRouter.js
@@ -4,17 +4,25 @@ const selectCellController = require('../controllers/cellMove/selectCell')
 const nonAssociationsController = require('../controllers/cellMove/viewNonAssociations')
 const offenderDetailsController = require('../controllers/cellMove/viewOffenderDetails')
 const cellSharingRiskAssessmentController = require('../controllers/cellMove/viewCellSharingAssessmentDetails')
-const moveValidationController = require('../controllers/cellMove/moveValidation')
+const { moveValidationFactory } = require('../controllers/cellMove/moveValidation')
 
 const router = express.Router({ mergeParams: true })
 
 const controller = ({ elite2Api, whereaboutsApi, logError }) => {
+  const {
+    index: moveValidationIndex,
+    post: moveValidationPost,
+    confirm: moveValidationConfirmation,
+  } = moveValidationFactory({ elite2Api, logError })
+
   router.get('/select-location', selectLocationController({ elite2Api, whereaboutsApi, logError }))
   router.get('/non-associations', nonAssociationsController({ elite2Api, logError }))
   router.get('/offender-details', offenderDetailsController({ elite2Api, logError }))
   router.get('/cell-sharing-risk-assessment-details', cellSharingRiskAssessmentController({ elite2Api, logError }))
   router.get('/select-cell', selectCellController({ elite2Api, whereaboutsApi, logError }))
-  router.get('/move-validation', moveValidationController({ elite2Api, logError }))
+  router.get('/move-validation', moveValidationIndex)
+  router.post('/move-validation', moveValidationPost)
+  router.get('/confirmation', moveValidationConfirmation)
 
   return router
 }
