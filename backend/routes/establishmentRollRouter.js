5e984751b8627e2e27d6f33275cb153c0c0f51d4
Nevelina Aleksandrova <nevelina@diginaga.online>
2020-10-15 08:56:55 +0100

    NN-2985: Move En route to server rendered

--- backend/routes/establishmentRollRouter.js
+++ backend/routes/establishmentRollRouter.js
@@ -1,11 +1,17 @@
 const express = require('express')
 
 const establishmentRollDashboardController = require('../controllers/establishmentRoll/dashboard')
+const enRouteController = require('../controllers/establishmentRoll/enRoute')
+
+const { movementsServiceFactory } = require('../services/movementsService')
 
 const router = express.Router({ mergeParams: true })
 
-const controller = ({ elite2Api, logError }) => {
+const controller = ({ elite2Api, systemOauthClient, logError }) => {
+  const movementsService = movementsServiceFactory(elite2Api, systemOauthClient)
+
   router.get('/', establishmentRollDashboardController({ elite2Api, logError }))
+  router.get('/en-route', enRouteController({ elite2Api, movementsService, logError }))
   return router
 }
 
