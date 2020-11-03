ace8c2e6fe56025bef8acd4bf797e6cc5ab1d600
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2020-11-03 13:01:52 +0000

    DCS-680 Moving test over to nock

--- backend/api/oauthApi.test.js
+++ backend/api/oauthApi.test.js
@@ -1,70 +1,38 @@
-const MockAdapter = require('axios-mock-adapter')
-const querystring = require('querystring')
+const nock = require('nock')
 const { oauthApiFactory } = require('./oauthApi')
 
 const clientId = 'clientId'
-const url = 'http://localhost'
+const url = 'http://localhost/oauth/token'
 const clientSecret = 'clientSecret'
-
-const encodeClientCredentials = () =>
-  Buffer.from(`${querystring.escape(clientId)}:${querystring.escape(clientSecret)}`).toString('base64')
 
 const client = jest.fn()
 const oauthApi = oauthApiFactory(client, { url, clientId, clientSecret })
-const mock = new MockAdapter(oauthApi.oauthAxios)
-
-const baseResponse = {
-  token_type: 'bearer',
-  expires_in: 59,
-  scope: 'write',
-  internalUser: true,
-  jti: 'bf5e8f62-1d2a-4126-96e2-a4ae91997ba6',
-}
+const mock = nock('http://localhost', { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
 
 describe('oathApi tests', () => {
-  let requestConfig
-  beforeAll(() => {
-    // Some hackery to catch the configuration used by axios to make the authentication / refresh request.
-    oauthApi.oauthAxios.interceptors.request.use(config => {
-      requestConfig = config
-      return config
-    })
-  })
-
   describe('refresh', () => {
-    let refreshResponse
-
-    beforeAll(() => {
-      mock.reset()
-      mock.onAny('oauth/token').reply(200, {
-        ...baseResponse,
-        access_token: 'newAccessToken',
-        refresh_token: 'newRefreshToken',
-      })
-
-      refreshResponse = oauthApi.refresh('refreshToken')
+    beforeEach(() => {
+      nock.cleanAll()
     })
 
     describe('should save tokens', () => {
-      it('should save access token', () =>
-        refreshResponse.then(response => {
-          expect(response.access_token).toEqual('newAccessToken')
-        }))
-
-      it('should save refresh token', () =>
-        refreshResponse.then(response => {
-          expect(response.refresh_token).toEqual('newRefreshToken')
-        }))
+      it('should save access token', async () => {
+        mock
+          .post('/oauth/token', { grant_type: 'refresh_token', refresh_token: 'refreshToken' })
+          .basicAuth({ user: clientId, pass: clientSecret })
+          .reply(200, {
+            token_type: 'bearer',
+            expires_in: 59,
+            scope: 'write',
+            internalUser: true,
+            jti: 'bf5e8f62-1d2a-4126-96e2-a4ae91997ba6',
+            access_token: 'newAccessToken',
+            refresh_token: 'newRefreshToken',
+          })
+        const response = await oauthApi.refresh('refreshToken')
+        expect(response.access_token).toEqual('newAccessToken')
+        expect(response.refresh_token).toEqual('newRefreshToken')
+      })
     })
-
-    it('should have set correct request configuration', () =>
-      refreshResponse.then(response => {
-        expect(requestConfig.method).toEqual('post')
-        expect(requestConfig.baseURL).toEqual(url)
-        expect(requestConfig.url).toEqual('http://localhost/oauth/token')
-        expect(requestConfig.data).toEqual('refresh_token=refreshToken&grant_type=refresh_token')
-        expect(requestConfig.headers.authorization).toEqual(`Basic ${encodeClientCredentials()}`)
-        expect(requestConfig.headers['Content-Type']).toEqual('application/x-www-form-urlencoded')
-      }))
   })
 })
