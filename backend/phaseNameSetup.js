2782ef1983528f2163e06181b591758cb60969d7
James King <james.king@digital.justice.gov.uk>
2020-11-05 09:58:35 +0000

    NN-2961: update readme, refactor code

--- backend/phaseNameSetup.js
+++ backend/phaseNameSetup.js
@@ -1,6 +1,9 @@
 /* eslint no-param-reassign: ["error", { "props": true, "ignorePropertyModificationsFor": ["app"] }] */
 
+const preprodText = 'This test version of DPS contains real data which may be up to two weeks old.'
+
 module.exports = (app, config) => {
   app.locals.phaseName = config.phaseName
   app.locals.phaseNameColour = config.phaseName === 'PRE_PRODUCTION' ? 'phase-banner-green' : 'phase-banner-blue'
+  app.locals.phaseNameText = config.phaseName === 'PRE_PRODUCTION' ? preprodText : ''
 }
