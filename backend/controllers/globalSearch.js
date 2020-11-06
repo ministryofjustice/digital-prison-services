467ab55c386984719a11b1227eb4fad24af78eeb
John Robertson <jsrobertson@gmail.com>
2020-11-06 14:31:33 +0000

    NN-3021 Remove globalSearch and factor into resultsPage

--- backend/controllers/globalSearch.js
+++ backend/controllers/globalSearch.js
@@ -1,11 +1,10 @@
 const moment = require('moment')
 const log = require('../log')
 const { isPrisonerIdentifier, putLastNameFirst } = require('../utils')
+const dobValidation = require('../shared/dobValidation')
 const {
   app: { notmEndpointUrl: dpsUrl },
 } = require('../config')
-
-const dobValidation = require('../shared/dobValidation')
 
 const globalSearchFactory = ({ paginationService, offenderSearchApi, logError }) => {
   const searchByOffender = (context, offenderNo, gender, location, dateOfBirth, pageLimit) =>
@@ -37,33 +36,6 @@ const globalSearchFactory = ({ paginationService, offenderSearchApi, logError })
     )
   }
 
-  const globalSearch = async (context, searchText, gender, location, dateOfBirth, pageLimit) => {
-    log.info(`In globalSearch, searchText=${searchText}`)
-    if (!searchText) {
-      return []
-    }
-    const text = searchText
-      .replace(/,/g, ' ')
-      .replace(/\s\s+/g, ' ')
-      .trim()
-    const data = await (isPrisonerIdentifier(text)
-      ? searchByOffender(context, text, gender, location, dateOfBirth, pageLimit)
-      : searchByName(context, text, gender, location, dateOfBirth, pageLimit))
-    log.info(data.length, 'globalSearch data received')
-
-    return data.map(offender => {
-      const formattedDateOfBirth =
-        offender.dateOfBirth && moment(offender.dateOfBirth, 'YYYY-MM-DD').format('DD/MM/YYYY')
-
-      return {
-        ...offender,
-        dateOfBirth: formattedDateOfBirth,
-        name: putLastNameFirst(offender.firstName, offender.lastName),
-        workingName: putLastNameFirst(offender.currentWorkingFirstName, offender.currentWorkingLastName),
-      }
-    })
-  }
-
   const indexPage = (req, res) => res.render('globalSearch/globalSearch.njk', { dpsUrl })
 
   const resultsPage = async (req, res) => {
@@ -86,9 +58,31 @@ const globalSearchFactory = ({ paginationService, offenderSearchApi, logError })
 
     try {
       if (searchText && !errors.length) {
-        const dateOfBirthFilter = dobIsValid ? dateOfBirth.format('YYYY-MM-DD') : undefined
+        log.info(`In globalSearch, searchText=${searchText}`)
 
-        results = await globalSearch(context, searchText, genderFilter, locationFilter, dateOfBirthFilter, pageLimit)
+        const dateOfBirthFilter = dobIsValid ? dateOfBirth.format('YYYY-MM-DD') : undefined
+        const text = searchText
+          .replace(/,/g, ' ')
+          .replace(/\s\s+/g, ' ')
+          .trim()
+
+        const searchData = await (isPrisonerIdentifier(text)
+          ? searchByOffender(context, text, genderFilter, locationFilter, dateOfBirthFilter, pageLimit)
+          : searchByName(context, text, genderFilter, locationFilter, dateOfBirthFilter, pageLimit))
+
+        log.info(searchData.length, 'globalSearch data received')
+
+        results = searchData.map(offender => {
+          const formattedDateOfBirth =
+            offender.dateOfBirth && moment(offender.dateOfBirth, 'YYYY-MM-DD').format('DD/MM/YYYY')
+
+          return {
+            ...offender,
+            dateOfBirth: formattedDateOfBirth,
+            name: putLastNameFirst(offender.firstName, offender.lastName),
+            workingName: putLastNameFirst(offender.currentWorkingFirstName, offender.currentWorkingLastName),
+          }
+        })
       }
 
       const totalRecords = context.responseHeaders?.['total-records'] || 0
@@ -101,13 +95,13 @@ const globalSearchFactory = ({ paginationService, offenderSearchApi, logError })
           filters,
         },
         openFilters: Boolean(Object.values(filters).filter(value => value && value !== 'ALL').length),
-        results,
         pagination: paginationService.getPagination(
           totalRecords,
           pageOffset,
           pageLimit,
           new URL(`${req.protocol}://${req.get('host')}${req.originalUrl}`)
         ),
+        results,
       })
     } catch (error) {
       if (error) logError(req.originalUrl, error, 'Sorry, the service is unavailable')
@@ -116,7 +110,7 @@ const globalSearchFactory = ({ paginationService, offenderSearchApi, logError })
     }
   }
 
-  return { globalSearch, indexPage, resultsPage }
+  return { indexPage, resultsPage }
 }
 
 module.exports = {
