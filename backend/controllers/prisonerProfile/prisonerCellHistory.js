5ba3114fa914287c67e5a9433f657f13bf7c2daa
John Robertson <jsrobertson@gmail.com>
2020-11-24 10:13:38 +0000

    NN-3052 Add moved in by to current location on cell history

--- backend/controllers/prisonerProfile/prisonerCellHistory.js
+++ backend/controllers/prisonerProfile/prisonerCellHistory.js
@@ -32,46 +32,39 @@ module.exports = ({ oauthApi, prisonApi, logError, page = 0 }) => async (req, re
       page,
       size: 10000,
     })
-    const userRoles = await oauthApi.userRoles(res.locals)
-    const canViewCellMoveButton = userRoles && userRoles.some(role => role.roleCode === 'CELL_MOVE')
+    const [userRoles, agencyData] = await Promise.all([oauthApi.userRoles(res.locals), getAgencyDetails(cells)])
+    const staff = await Promise.all(cells.map(cell => prisonApi.getStaffDetails(res.locals, cell.movementMadeBy)))
 
-    // Collect a list of the unique agencies, to remove
-    // the need to make duplicate API calls
-    const agencyData = await getAgencyDetails(cells)
+    const cellData = cells.map(cell => {
+      const staffDetails = staff.find(user => cell.movementMadeBy === user.username)
+      const agency = agencyData.find(agencyDetails => cell.agencyId === agencyDetails.agencyId)
 
-    const currentLocation = cells.find(cell => cell.assignmentEndDateTime === undefined)
+      return {
+        establishment: agency.description,
+        location: extractLocation(cell.description, cell.agencyId),
+        movedIn: cell.assignmentDateTime && formatTimestampToDateTime(cell.assignmentDateTime),
+        movedOut: cell.assignmentEndDateTime && formatTimestampToDateTime(cell.assignmentEndDateTime),
+        assignmentDateTime: moment(cell.assignmentDateTime).format('YYYY-MM-DDTHH:mm:ss'),
+        assignmentEndDateTime: cell.assignmentEndDateTime
+          ? moment(cell.assignmentEndDateTime).format('YYYY-MM-DDTHH:mm:ss')
+          : undefined,
+        livingUnitId: cell.livingUnitId,
+        agencyId: cell.agencyId,
+        movedInBy: formatName(staffDetails.firstName, staffDetails.lastName),
+      }
+    })
+
+    const currentLocation = cellData.find(cell => cell.assignmentEndDateTime === undefined)
     const occupants =
       (currentLocation && (await prisonApi.getInmatesAtLocation(res.locals, currentLocation.livingUnitId, {}))) || []
 
-    const today = moment().format('YYYY-MM-DDTHH:mm:ss')
-
-    const cellData = await Promise.all(
-      cells
-        .filter(cell => cell.assignmentEndDateTime)
-        .sort((left, right) => sortByDateTime(right.assignmentDateTime, left.assignmentDateTime))
-        .map(async cell => {
-          const staffDetails = await prisonApi.getStaffDetails(res.locals, cell.movementMadeBy)
-          const agency = agencyData.find(agencyDetails => cell.agencyId === agencyDetails.agencyId)
-
-          return {
-            establishment: agency.description,
-            location: extractLocation(cell.description, cell.agencyId),
-            movedIn: cell.assignmentDateTime && formatTimestampToDateTime(cell.assignmentDateTime),
-            movedOut: cell.assignmentEndDateTime && formatTimestampToDateTime(cell.assignmentEndDateTime),
-            assignmentDateTime: moment(cell.assignmentDateTime).format('YYYY-MM-DDTHH:mm:ss'),
-            assignmentEndDateTime: cell.assignmentEndDateTime
-              ? moment(cell.assignmentEndDateTime).format('YYYY-MM-DDTHH:mm:ss')
-              : today,
-            livingUnitId: cell.livingUnitId,
-            agencyId: cell.agencyId,
-            movedBy: formatName(staffDetails.firstName, staffDetails.lastName),
-          }
-        })
-    )
+    const previousLocations = cellData
+      .filter(cell => cell.assignmentEndDateTime)
+      .sort((left, right) => sortByDateTime(right.assignmentDateTime, left.assignmentDateTime))
 
     return res.render('prisonerProfile/prisonerCellHistory.njk', {
-      cellHistoryGroupedByAgency: hasLength(cellData)
-        ? Object.entries(groupBy(cellData, 'establishment')).map(([key, value]) => {
+      cellHistoryGroupedByAgency: hasLength(previousLocations)
+        ? Object.entries(groupBy(previousLocations, 'establishment')).map(([key, value]) => {
             const fromDateString = formatTimestampToDate(value.slice(-1)[0].assignmentDateTime)
             const toDateString = formatTimestampToDate(value[0].assignmentEndDateTime)
 
@@ -82,18 +75,7 @@ module.exports = ({ oauthApi, prisonApi, logError, page = 0 }) => async (req, re
             }
           })
         : [],
-      currentLocation: {
-        establishment: agencyData.find(agencyDetails => currentLocation.agencyId === agencyDetails.agencyId)
-          .description,
-        location: extractLocation(currentLocation.description, currentLocation.agencyId),
-        movedIn: currentLocation.assignmentDateTime && formatTimestampToDateTime(currentLocation.assignmentDateTime),
-        assignmentDateTime: moment(currentLocation.assignmentDateTime).format('YYYY-MM-DDTHH:mm:ss'),
-        assignmentEndDateTime: currentLocation.assignmentEndDateTime
-          ? moment(currentLocation.assignmentEndDate).format('YYYY-MM-DDTHH:mm:ss')
-          : today,
-        livingUnitId: currentLocation.livingUnitId,
-        agencyId: currentLocation.agencyId,
-      },
+      currentLocation,
       occupants: occupants.filter(occupant => occupant.offenderNo !== offenderNo).map(occupant => {
         return {
           name: putLastNameFirst(occupant.firstName, occupant.lastName),
@@ -104,7 +86,7 @@ module.exports = ({ oauthApi, prisonApi, logError, page = 0 }) => async (req, re
       profileUrl: `/prisoner/${offenderNo}`,
       breadcrumbPrisonerName: putLastNameFirst(firstName, lastName),
       changeCellLink: `/prisoner/${offenderNo}/cell-move/select-location`,
-      canViewCellMoveButton,
+      canViewCellMoveButton: userRoles && userRoles.some(role => role.roleCode === 'CELL_MOVE'),
       dpsUrl,
     })
   } catch (error) {
