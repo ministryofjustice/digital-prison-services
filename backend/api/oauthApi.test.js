acae31c7d3e4d16a310102cc673563a7891394c0
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2021-01-05 10:15:43 +0000

    NN-3122 update jest and contentful

--- backend/api/oauthApi.test.js
+++ backend/api/oauthApi.test.js
@@ -1,70 +1,115 @@
-const MockAdapter = require('axios-mock-adapter')
-const querystring = require('querystring')
+const nock = require('nock')
 const { oauthApiFactory } = require('./oauthApi')
 
 const clientId = 'clientId'
-const url = 'http://localhost'
+const url = 'http://localhost/'
 const clientSecret = 'clientSecret'
 
-const encodeClientCredentials = () =>
-  Buffer.from(`${querystring.escape(clientId)}:${querystring.escape(clientSecret)}`).toString('base64')
-
-const client = jest.fn()
+const client = {}
 const oauthApi = oauthApiFactory(client, { url, clientId, clientSecret })
-const mock = new MockAdapter(oauthApi.oauthAxios)
+const mock = nock(url, { reqheaders: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+const context = { some: 'context' }
 
-const baseResponse = {
-  token_type: 'bearer',
-  expires_in: 59,
-  scope: 'write',
-  internalUser: true,
-  jti: 'bf5e8f62-1d2a-4126-96e2-a4ae91997ba6',
-}
-
-describe('oathApi tests', () => {
-  let requestConfig
-  beforeAll(() => {
-    // Some hackery to catch the configuration used by axios to make the authentication / refresh request.
-    oauthApi.oauthAxios.interceptors.request.use(config => {
-      requestConfig = config
-      return config
-    })
+describe('oauthApi tests', () => {
+  beforeEach(() => {
+    nock.cleanAll()
   })
 
   describe('refresh', () => {
-    let refreshResponse
+    it('should save access token', async () => {
+      mock
+        .post('/oauth/token', { grant_type: 'refresh_token', refresh_token: 'refreshToken' })
+        .basicAuth({ user: clientId, pass: clientSecret })
+        .reply(200, {
+          token_type: 'bearer',
+          expires_in: 59,
+          scope: 'write',
+          internalUser: true,
+          jti: 'bf5e8f62-1d2a-4126-96e2-a4ae91997ba6',
+          access_token: 'newAccessToken',
+          refresh_token: 'newRefreshToken',
+        })
+      const response = await oauthApi.refresh('refreshToken')
+      expect(response.access_token).toEqual('newAccessToken')
+      expect(response.refresh_token).toEqual('newRefreshToken')
+    })
+  })
 
-    beforeAll(() => {
-      mock.reset()
-      mock.onAny('oauth/token').reply(200, {
-        ...baseResponse,
-        access_token: 'newAccessToken',
-        refresh_token: 'newRefreshToken',
+  describe('currentUser', () => {
+    const userDetails = { bob: 'hello there' }
+    let actual
+
+    beforeEach(() => {
+      client.get = jest.fn().mockReturnValue({
+        then: () => userDetails,
       })
-
-      refreshResponse = oauthApi.refresh('refreshToken')
+      actual = oauthApi.currentUser(context)
     })
 
-    describe('should save tokens', () => {
-      it('should save access token', () =>
-        refreshResponse.then(response => {
-          expect(response.access_token).toEqual('newAccessToken')
-        }))
+    it('should return user details from endpoint', () => {
+      expect(actual).toEqual(userDetails)
+    })
+    it('should call user endpoint', () => {
+      expect(client.get).toBeCalledWith(context, '/api/user/me')
+    })
+  })
 
-      it('should save refresh token', () =>
-        refreshResponse.then(response => {
-          expect(response.refresh_token).toEqual('newRefreshToken')
-        }))
+  describe('userDetails', () => {
+    const userDetails = { bob: 'hello there' }
+    const username = 'bob'
+    let actual
+
+    beforeEach(() => {
+      client.get = jest.fn().mockReturnValue({
+        then: () => userDetails,
+      })
+      actual = oauthApi.userDetails(context, username)
     })
 
-    it('should have set correct request configuration', () =>
-      refreshResponse.then(response => {
-        expect(requestConfig.method).toEqual('post')
-        expect(requestConfig.baseURL).toEqual(url)
-        expect(requestConfig.url).toEqual('http://localhost/oauth/token')
-        expect(requestConfig.data).toEqual('refresh_token=refreshToken&grant_type=refresh_token')
-        expect(requestConfig.headers.authorization).toEqual(`Basic ${encodeClientCredentials()}`)
-        expect(requestConfig.headers['Content-Type']).toEqual('application/x-www-form-urlencoded')
-      }))
+    it('should return user details from endpoint', () => {
+      expect(actual).toEqual(userDetails)
+    })
+    it('should call user endpoint', () => {
+      expect(client.get).toBeCalledWith(context, `/api/user/${username}`)
+    })
+  })
+
+  describe('userEmail', () => {
+    const userEmail = { email: 'bob@local' }
+    const username = 'bob'
+    let actual
+
+    beforeEach(() => {
+      client.get = jest.fn().mockReturnValue({
+        then: () => userEmail,
+      })
+      actual = oauthApi.userEmail(context, username)
+    })
+
+    it('should return user details from endpoint', () => {
+      expect(actual).toEqual(userEmail)
+    })
+    it('should call user endpoint', () => {
+      expect(client.get).toBeCalledWith(context, `/api/user/${username}/email`)
+    })
+  })
+
+  describe('currentRoles', () => {
+    const roles = { bob: 'hello there' }
+    let actual
+
+    beforeEach(() => {
+      client.get = jest.fn().mockReturnValue({
+        then: () => roles,
+      })
+      actual = oauthApi.userRoles(context)
+    })
+
+    it('should return roles from endpoint', () => {
+      expect(actual).toEqual(roles)
+    })
+    it('should call user endpoint', () => {
+      expect(client.get).toBeCalledWith(context, '/api/user/me/roles')
+    })
   })
 })
