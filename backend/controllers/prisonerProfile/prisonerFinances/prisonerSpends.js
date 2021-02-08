47972283de3e5be25b4ec24af3bdd59dc43664e7
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2021-02-08 12:25:28 +0000

    NN-3158 Display related transactions

--- backend/controllers/prisonerProfile/prisonerFinances/prisonerSpends.js
+++ backend/controllers/prisonerProfile/prisonerFinances/prisonerSpends.js
@@ -1,4 +1,5 @@
 const createTransactionViewModel = require('../../../shared/createTransactionViewModel')
+const { formatTimestampToDate, sortByDateTime } = require('../../../utils')
 
 module.exports = ({ prisonApi, prisonerFinanceService }) => async (req, res) => {
   const { month, year } = req.query
@@ -13,10 +14,46 @@ module.exports = ({ prisonApi, prisonerFinanceService }) => async (req, res) => 
 
     const uniqueAgencyIds = [...new Set(allTransactionsForDateRange.map(transaction => transaction.agencyId))]
     const prisons = await Promise.all(uniqueAgencyIds.map(agencyId => prisonApi.getAgencyDetails(res.locals, agencyId)))
+    const batchTransactionsOnly = transaction => transaction?.relatedOffenderTransactions?.length
+
+    const relatedTransactions = allTransactionsForDateRange
+      .filter(batchTransactionsOnly)
+      .flatMap(batchTransaction => {
+        const related = batchTransaction.relatedOffenderTransactions?.map(relatedTransaction => ({
+          id: batchTransaction.id,
+          entryDate: batchTransaction.entryDate,
+          agencyId: batchTransaction.agencyId,
+          penceAmount: relatedTransaction.payAmount,
+          entryDescription: `${relatedTransaction.paymentDescription} from ${formatTimestampToDate(
+            relatedTransaction.calendarDate
+          )}`,
+          postingType: 'CR',
+          calendarDate: relatedTransaction.calendarDate,
+        }))
+
+        const sortedByLatestDate = related.sort((left, right) => sortByDateTime(left.calendarDate, right.calendarDate))
+
+        const accumulatedFromBatch = related.reduce((result, current) => current.penceAmount + result, 0)
+        const startingBalanceForBatch = batchTransaction.currentBalance - accumulatedFromBatch
+
+        let runningTotal = 0
+        return sortedByLatestDate.map(current => {
+          runningTotal += current.penceAmount
+          return {
+            ...current,
+            currentBalance: startingBalanceForBatch + runningTotal,
+          }
+        })
+      })
+      .reverse()
+
+    const transactionsExcludingBatch = allTransactionsForDateRange.filter(
+      transaction => !batchTransactionsOnly(transaction)
+    )
 
     return res.render('prisonerProfile/prisonerFinance/spends.njk', {
       ...templateData,
-      spendsRows: createTransactionViewModel(allTransactionsForDateRange, prisons),
+      spendsRows: createTransactionViewModel([...transactionsExcludingBatch, ...relatedTransactions], prisons),
     })
   } catch (error) {
     res.locals.redirectUrl = `/prisoner/${offenderNo}`
