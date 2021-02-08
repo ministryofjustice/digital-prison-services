586d65aeede6f50e7e4393ec27bf17f30b025dd4
Jamie Draper <draper65@googlemail.com>
2021-02-08 13:26:02 +0000

    NN-3145 filter cleared holdings out of the list (#1498)
    Co-authored-by: JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>

--- backend/controllers/prisonerProfile/prisonerFinances/prisonerPrivateCash.js
+++ backend/controllers/prisonerProfile/prisonerFinances/prisonerPrivateCash.js
@@ -14,9 +14,9 @@ module.exports = ({ prisonApi, prisonerFinanceService }) => async (req, res) => 
       prisonerFinanceService.getTemplateData(res.locals, offenderNo, accountCode, month, year),
     ])
 
-    const sortedPendingTransactions = [...addHoldFunds, ...withheldFunds].sort((left, right) =>
-      sortByDateTime(right.entryDate, left.entryDate)
-    )
+    const sortedPendingTransactions = [...addHoldFunds, ...withheldFunds]
+      .sort((left, right) => sortByDateTime(right.entryDate, left.entryDate))
+      .filter(transaction => !transaction.holdClearFlag)
 
     const pendingBalanceInPence = sortedPendingTransactions.reduce(
       (acc, current) => (current.postingType === 'DR' ? acc - current.penceAmount : acc + current.penceAmount),
@@ -32,13 +32,14 @@ module.exports = ({ prisonApi, prisonerFinanceService }) => async (req, res) => 
         [...sortedPendingTransactions, ...allTransactionsForDateRange].map(transaction => transaction.agencyId)
       ),
     ]
+
     const prisons = await Promise.all(uniqueAgencyIds.map(agencyId => prisonApi.getAgencyDetails(res.locals, agencyId)))
 
     return res.render('prisonerProfile/prisonerFinance/privateCash.njk', {
       ...templateData,
       nonPendingRows: createTransactionViewModel(nonPendingTransactions, prisons),
       pendingBalance: formatCurrency(pendingBalanceInPence / 100),
-      pendingRows: [], // createTransactionViewModel(sortedPendingTransactions, prisons, false),
+      pendingRows: createTransactionViewModel(sortedPendingTransactions, prisons, false, true),
     })
   } catch (error) {
     res.locals.redirectUrl = `/prisoner/${offenderNo}`
--- backend/shared/createTransactionViewModel.js
+++ backend/shared/createTransactionViewModel.js
@@ -1,14 +1,18 @@
 const { formatCurrency, formatTimestampToDate } = require('../utils')
 
-module.exports = (transactions, prisons, showBalance = true) =>
+module.exports = (transactions, prisons, showBalance = true, mergeAmounts = false) =>
   transactions.map(transaction => {
     const { description: prisonName } = prisons.find(agency => transaction.agencyId === agency.agencyId)
     const { penceAmount, currentBalance } = transaction
 
     return [
       { text: transaction.entryDate && formatTimestampToDate(transaction.entryDate) },
-      { text: transaction.postingType === 'CR' ? formatCurrency(penceAmount / 100) : '' },
-      { text: transaction.postingType === 'DR' ? formatCurrency(penceAmount / 100) : '' },
+      ...(mergeAmounts
+        ? [{ text: (penceAmount && formatCurrency(penceAmount / 100)) || '' }]
+        : [
+            { text: transaction.postingType === 'CR' ? formatCurrency(penceAmount / 100) : '' },
+            { text: transaction.postingType === 'DR' ? formatCurrency(penceAmount / 100) : '' },
+          ]),
       ...(showBalance ? [{ text: formatCurrency(currentBalance / 100) }] : []),
       { text: transaction.entryDescription },
       { text: prisonName },
