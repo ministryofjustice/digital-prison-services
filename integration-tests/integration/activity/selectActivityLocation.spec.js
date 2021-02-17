a57a1bf4bf18d6988f9124089b5fa67dfe7a9ba6
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2021-02-17 13:38:23 +0000

    NN-3153 issue with page title not showing

--- integration-tests/integration/activity/selectActivityLocation.spec.js
+++ integration-tests/integration/activity/selectActivityLocation.spec.js
@@ -1,23 +1,33 @@
+const caseload = 'MDI'
+const date = new Date().toISOString().split('T')[0]
+
 context('Select activity location', () => {
   const headerText = 'View by activity or appointment location'
 
-  before(() => {
-    cy.clearCookies()
+  beforeEach(() => {
     cy.task('reset')
-    cy.task('stubLogin', { username: 'ITAG_USER', caseload: 'MDI' })
+    cy.task('stubGroups', { id: caseload })
+    cy.task('stubLogin', { username: 'ITAG_USER', caseload })
     cy.login()
+    cy.task('stubActivityLocations')
+    cy.task('stubGetAbsenceReasons')
+    cy.task('stubActivityLocations')
   })
 
   it('should redirect to the activity lists page', () => {
-    cy.task('stubActivityLocations')
+    cy.task('stubGetActivityList', { caseload, locationId: 2, timeSlot: 'AM', date })
+    cy.task('stubGetAttendance', { caseload, locationId: 2, timeSlot: 'AM', date })
+
     cy.visit('/manage-prisoner-whereabouts/select-location')
 
     cy.get('h1').contains(headerText)
-    cy.get('#current-location').select('loc1')
+    cy.get('#current-location').select('loc2')
 
     cy.get('h1').contains(headerText)
     cy.get('[type="submit"]').click()
 
     cy.location('pathname').should('eq', '/manage-prisoner-whereabouts/activity-results')
+
+    cy.get('h1').contains('loc2')
   })
 })
