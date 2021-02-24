f504c7823580207e5f08e98711e543c48c063008
John Robertson <jsrobertson@users.noreply.github.com>
2021-02-24 14:41:31 +0000

    Revert "Revert "Revert "Revert "NN-3008 CSRA history page (#1532)" (#1542)" (#1544)" (#1547)"
    This reverts commit 224bf89fe9f437d11f2d79341c0ecd518683618f.

--- integration-tests/mockApis/prisonApi.js
+++ integration-tests/mockApis/prisonApi.js
@@ -200,6 +200,21 @@ module.exports = {
       },
     })
   },
+  stubCsraAssessmentsForPrisoner: (assessments = []) => {
+    return stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/api/offender-assessments/CSR\\?.+?',
+      },
+      response: {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+        },
+        jsonBody: assessments,
+      },
+    })
+  },
   stubIepSummaryForBookingIds: results => {
     return stubFor({
       request: {
