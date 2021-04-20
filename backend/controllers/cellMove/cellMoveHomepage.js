15108ff28f79781dd184c7e5bd369e3f322c9177
John Robertson <jsrobertson@users.noreply.github.com>
2021-04-20 12:14:39 +0100

    NN-3263 Add new cell move homepage (#1626)

---
+++ backend/controllers/cellMove/cellMoveHomepage.js
@@ -0,0 +1,58 @@
+const whereaboutsTasks = [
+  {
+    id: 'search-for-prisoner',
+    heading: 'Search for a prisoner',
+    description: 'Change someone’s cell after searching for them using their name or prison number.',
+    href: '/change-someones-cell/prisoner-search',
+    roles: null,
+    enabled: true,
+  },
+  {
+    id: 'view-residential-location',
+    heading: 'View residential location',
+    description: 'View all prisoners in a residential location. You can view their cell history and change their cell.',
+    href: '/change-someones-cell/view-residential-location',
+    roles: null,
+    enabled: true,
+  },
+  {
+    id: 'create-space',
+    heading: 'Move someone temporarily out of a cell',
+    description:
+      'Create a space for another prisoner by moving someone out of a cell temporarily. You will need to allocate a cell to them later.',
+    href: '/change-someones-cell/temporary-move',
+    roles: null,
+    enabled: true,
+  },
+  {
+    id: 'view-history',
+    heading: 'View 7 day cell move history',
+    description:
+      'View all cell moves completed over the last 7 days in [prison name]. Note that the name will be the caseload that the user has selected.',
+    href: '/change-someones-cell/recent-cell-moves',
+    roles: null,
+    enabled: true,
+  },
+  {
+    id: 'no-cell-allocated',
+    heading: 'No cell allocated',
+    description: 'View people who do not currently have a cell allocated having been temporarily moved out of a cell.',
+    href: '/establishment-roll/no-cell-allocated',
+    roles: null,
+    enabled: true,
+  },
+]
+
+module.exports = oauthApi => async (req, res) => {
+  const userRoles = await oauthApi.userRoles(res.locals)
+
+  const canViewCellMoveButton = userRoles?.some(role => role.roleCode === 'CELL_MOVE')
+
+  if (canViewCellMoveButton) {
+    return res.render('cellMove/cellMoveHomepage', {
+      tasks: whereaboutsTasks.filter(task => task.enabled).map(({ roles, enabled, ...task }) => task),
+    })
+  }
+
+  return res.render('notFound', { url: '/' })
+}
---
+++ integration-tests/integration/cellMove/cellMoveHomepage.spec.js
@@ -0,0 +1,23 @@
+context('Cell move homepage', () => {
+  beforeEach(() => {
+    cy.clearCookies()
+    cy.task('reset')
+  })
+
+  describe('Tasks', () => {
+    beforeEach(() => {
+      cy.task('stubLogin', { username: 'ITAG_USER', caseload: 'MDI', roles: [{ roleCode: 'CELL_MOVE' }] })
+      cy.login()
+    })
+
+    it('should non role specific tasks', () => {
+      cy.visit('/change-someones-cell')
+
+      cy.get('[data-test="search-for-prisoner"]').should('exist')
+      cy.get('[data-test="view-residential-location"]').should('exist')
+      cy.get('[data-test="create-space"]').should('exist')
+      cy.get('[data-test="view-history"]').should('exist')
+      cy.get('[data-test="no-cell-allocated"]').should('exist')
+    })
+  })
+})
