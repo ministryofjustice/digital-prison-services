14f7ebb2a9f6c4e828bfcb95c6ecea6ab5687e4b
sp-watson <steve.watson@digital,justice.gov.uk>
2021-05-28 11:45:09 +0100

    NN-3351: Integration tests for appointment deleted page

---
+++ integration-tests/integration/appointments/appointmentDeleted.spec.js
@@ -0,0 +1,37 @@
+const DeletedSingleAppointmentPage = require('../../pages/appointments/deletedSingleAppointmentPage')
+const DeletedMultipleAppointmentsPage = require('../../pages/appointments/deletedMultipleAppointmentsPage')
+
+context('Deleted appointment page', () => {
+  before(() => {
+    cy.clearCookies()
+    cy.task('resetAndStubTokenVerification')
+    cy.task('stubLogin', { username: 'ITAG_USER', caseload: 'WWI' })
+    cy.login()
+  })
+
+  beforeEach(() => {
+    Cypress.Cookies.preserveOnce('hmpps-session-dev')
+  })
+
+  it('A user successfully deletes a single appointment then navigates to view all appointments page', () => {
+    cy.visit(`/appointment-details/deleted`)
+    const deletedSingleAppointmentPage = DeletedSingleAppointmentPage.verifyOnPage()
+
+    deletedSingleAppointmentPage.finishLink()
+      .should('have.attr', 'href')
+      .then(href => {
+        expect(href).to.equal('/view-all-appointments')
+      })
+  })
+
+  it('A user successfully deletes multiple appointments then navigates to view all appointments page', () => {
+    cy.visit(`/appointment-details/deleted?multipleDeleted=true`)
+    const deletedMultipleAppointmentPage = DeletedMultipleAppointmentsPage.verifyOnPage()
+
+    deletedMultipleAppointmentPage.finishLink()
+      .should('have.attr', 'href')
+      .then(href => {
+        expect(href).to.equal('/view-all-appointments')
+      })
+  })
+})
