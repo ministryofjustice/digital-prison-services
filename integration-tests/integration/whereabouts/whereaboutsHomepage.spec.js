fb1a2ec8b82f3df1182bf6057afe3bd6dd853941
Akeem Faulkner <43253053+ake3mio@users.noreply.github.com>
2021-07-02 13:15:10 +0100

    NN-3446: Updates main tile menu and Whereabouts menu (#1750)

--- integration-tests/integration/whereabouts/whereaboutsHomepage.spec.js
+++ integration-tests/integration/whereabouts/whereaboutsHomepage.spec.js
@@ -2,8 +2,6 @@ context('Whereabouts homepage', () => {
   beforeEach(() => {
     cy.clearCookies()
     cy.task('reset')
-
-    cy.task('stubUserMeRoles')
   })
 
   describe('Tasks', () => {
@@ -13,6 +11,7 @@ context('Whereabouts homepage', () => {
     })
 
     it('should non role specific tasks', () => {
+      cy.task('stubUserMeRoles')
       cy.visit('/manage-prisoner-whereabouts')
 
       cy.get('[data-test="view-residential-location"]').should('exist')
@@ -20,6 +19,20 @@ context('Whereabouts homepage', () => {
       cy.get('[data-test="view-all-appointments"]').should('exist')
       cy.get('[data-test="view-unaccounted-for"]').should('exist')
       cy.get('[data-test="view-attendance-statistics"]').should('exist')
+      cy.get('[data-test="view-bulk-appointments"]').should('not.exist')
+      cy.get('[data-test="view-covid-units"]').should('not.exist')
+    })
+
+    it('should show covid unit task', () => {
+      cy.task('stubUserMeRoles', [{ roleCode: 'PRISON' }])
+      cy.visit('/manage-prisoner-whereabouts')
+      cy.get('[data-test="view-covid-units"]').should('exist')
+    })
+
+    it('should show bulk appointments task', () => {
+      cy.task('stubUserMeRoles', [{ roleCode: 'BULK_APPOINTMENTS' }])
+      cy.visit('/manage-prisoner-whereabouts')
+      cy.get('[data-test="view-bulk-appointments"]').should('exist')
     })
   })
 })
