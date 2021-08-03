c200cc2f50d30f994c57d94ff450036d108d7898
Jamie Draper <draper65@googlemail.com>
2021-08-03 12:58:02 +0100

    Revert "Nn 3564 learning history accordion (#1776)"
    This reverts commit 3cd2c62d37a8bb3d0b881f0617f2d6ee83a55f9c.

--- backend/controllers/prisonerProfile/prisonerWorkAndSkills.ts
+++ backend/controllers/prisonerProfile/prisonerWorkAndSkills.ts
@@ -9,17 +9,15 @@ export default ({ prisonerProfileService, esweService }) =>
       return res.redirect(`/prisoner/${offenderNo}`)
     }
 
-    const [prisonerProfileData, functionalSkillLevels, learningHistory] = await Promise.all(
+    const [prisonerProfileData, functionalSkillLevels] = await Promise.all(
       [
         prisonerProfileService.getPrisonerProfileData(res.locals, offenderNo),
         esweService.getLearnerLatestAssessments(offenderNo),
-        esweService.getLearnerEducation(offenderNo),
       ].map((apiCall) => logErrorAndContinue(apiCall))
     )
 
     return res.render('prisonerProfile/prisonerWorkAndSkills/prisonerWorkAndSkills.njk', {
       prisonerProfileData,
       functionalSkillLevels,
-      learningHistory,
     })
   }
