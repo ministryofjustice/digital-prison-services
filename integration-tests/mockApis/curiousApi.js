73172f42717884dbcabfd9ec4aa4a7006e157dab
Jamie Draper <draper65@googlemail.com>
2021-08-03 14:01:38 +0100

    Revert "Revert "Nn 3564 learning history accordion (#1776)" (#1778)" (#1779)
    This reverts commit 8f502989d479db8ff01688a0550653a3b2782d2c.

--- integration-tests/mockApis/curiousApi.js
+++ integration-tests/mockApis/curiousApi.js
@@ -15,4 +15,18 @@ module.exports = {
         jsonBody: learnerAssessments || [],
       },
     }),
+  stubLearnerEducation: (learnerHistory, status = 200) =>
+    stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: `/sequation-virtual-campus2-api/learnerEducation/.+?`,
+      },
+      response: {
+        status,
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+        },
+        jsonBody: learnerHistory || [],
+      },
+    }),
 }
