ef007f7103942502b2d9ed10364b6034c4f43b4e
Richard Popple <richard.popple@digital.justice.gov.uk>
2021-09-24 12:04:39 +0100

    NN-3643 replacing more references to login and logout

--- integration-tests/integration/signIn/signIn.spec.js
+++ integration-tests/integration/signIn/signIn.spec.js
@@ -15,71 +15,65 @@ context('Login functionality', () => {
   })
 
   it('Root (/) redirects to the auth login page if not logged in', () => {
-    cy.task('stubLoginPage')
+    cy.task('stubSignInPage')
     cy.visit('/')
     cy.url().should('include', 'authorize')
     cy.get('h1').should('contain.text', 'Sign in')
   })
 
   it('Login page redirects to the auth login page if not logged in', () => {
-    cy.task('stubLoginPage')
+    cy.task('stubSignInPage')
     cy.visit('/login')
     cy.url().should('include', 'authorize')
     cy.get('h1').should('contain.text', 'Sign in')
   })
 
   it('Page redirects to the auth login page if not logged in', () => {
-    cy.task('stubLogin', {})
+    cy.task('stubSignIn', {})
     cy.visit('/login')
     cy.url().should('include', 'authorize')
     cy.get('h1').should('contain.text', 'Sign in')
   })
 
   it('Logout takes user to login page', () => {
-    cy.task('stubLogin', {})
-    cy.login()
+    cy.task('stubSignIn', {})
+    cy.signIn()
     HomePage.verifyOnPage()
 
     // can't do a visit here as cypress requires only one domain
-    cy.request('/auth/logout')
-      .its('body')
-      .should('contain', 'Sign in')
+    cy.request('/auth/logout').its('body').should('contain', 'Sign in')
   })
 
   it('Token verification failure takes user to sign in page', () => {
-    cy.task('stubLogin', {})
-    cy.login()
+    cy.task('stubSignIn', {})
+    cy.signIn()
     HomePage.verifyOnPage()
     cy.task('stubVerifyToken', false)
 
     // can't do a visit here as cypress requires only one domain
-    cy.request('/')
-      .its('body')
-      .should('contain', 'Sign in')
+    cy.request('/').its('body').should('contain', 'Sign in')
   })
 
   it('Token verification failure clears user session', () => {
-    cy.task('stubLogin', {})
-    cy.login()
+    cy.task('stubSignIn', {})
+    cy.signIn()
     const homePage = HomePage.verifyOnPage()
     homePage.loggedInName().contains('J. Stuart')
     cy.task('stubVerifyToken', false)
 
     // can't do a visit here as cypress requires only one domain
-    cy.request('/')
-      .its('body')
-      .should('contain', 'Sign in')
+    cy.request('/').its('body').should('contain', 'Sign in')
 
     cy.task('stubVerifyToken', true)
     cy.task('stubUserMe', { name: 'Bobby Brown' })
-    cy.login()
+    cy.signIn()
 
     homePage.loggedInName().contains('B. Brown')
   })
 
   it('Log in as ordinary user', () => {
-    cy.task('stubLogin', {})
-    cy.login()
+    cy.task('stubSignIn', {})
+    cy.signIn()
     HomePage.verifyOnPage()
   })
 })
