4ce48509af9b08e7bce1c3193ee4a795238f1648
Jamie Draper <draper65@googlemail.com>
2021-09-29 10:02:04 +0100

    NN-1867 add transfers and releases (#1852)
    * NN-1867 add transfers and releases
    
    * NN-1867 fix lint

--- integration-tests/integration/whereabouts/scheduledMoves.spec.js
+++ integration-tests/integration/whereabouts/scheduledMoves.spec.js
@@ -4,6 +4,7 @@ const agencyDetails = { agencyId: 'MDI', description: 'Moorland (HMP & YOI)' }
 const movementReasons = [
   { code: '1', description: 'Visit Dying Relative' },
   { code: 'CRT', description: 'Court Appearance' },
+  { code: 'TRN', description: 'Normal Transfer' },
 ]
 
 const courtEvents = [
@@ -25,6 +26,55 @@ const courtEvents = [
     judgeName: null,
     directionCode: 'OUT',
     commentText: null,
+    bookingActiveFlag: true,
+    bookingInOutStatus: 'IN',
+  },
+]
+
+const releaseEvents = [
+  {
+    offenderNo: 'G3854XD',
+    createDateTime: '2016-11-07T15:13:59.268001',
+    eventId: 320696788,
+    fromAgency: 'MDI',
+    fromAgencyDescription: 'Moorland (HMP & YOI)',
+    releaseDate: '2021-09-29',
+    approvedReleaseDate: null,
+    eventClass: 'EXT_MOV',
+    eventStatus: 'SCH',
+    movementTypeCode: 'REL',
+    movementTypeDescription: 'Release',
+    movementReasonCode: 'CR',
+    movementReasonDescription: 'Conditional Release (CJA91) -SH Term>1YR',
+    commentText: null,
+    bookingActiveFlag: true,
+    bookingInOutStatus: 'OUT',
+  },
+]
+
+const transferEvents = [
+  {
+    offenderNo: 'G5966UI',
+    createDateTime: '2021-09-22T10:26:50.745683',
+    eventId: 449330572,
+    fromAgency: 'MDI',
+    fromAgencyDescription: 'Moorland (HMP & YOI)',
+    toAgency: 'LEI',
+    toAgencyDescription: 'Leeds (HMP)',
+    toCity: null,
+    eventStatus: 'SCH',
+    eventClass: 'EXT_MOV',
+    eventType: 'TRN',
+    eventSubType: 'NOTR',
+    eventDate: '2021-09-29',
+    startTime: '2021-09-29T10:00:00',
+    endTime: null,
+    outcomeReasonCode: null,
+    judgeName: null,
+    engagementCode: null,
+    escortCode: 'GEOAME',
+    performanceCode: null,
+    directionCode: 'OUT',
     bookingActiveFlag: true,
     bookingInOutStatus: 'IN',
   },
@@ -80,9 +130,25 @@ const prisonerSearchDetails = [
     cellLocation: '1-2-006',
     alerts,
   },
+  {
+    prisonerNumber: 'G3854XD',
+    bookingId: 3,
+    firstName: 'DAVE',
+    lastName: 'SHAVE',
+    cellLocation: '1-2-008',
+    alerts,
+  },
+  {
+    prisonerNumber: 'G5966UI',
+    bookingId: 2,
+    firstName: 'MARK',
+    lastName: 'SHARK',
+    cellLocation: '1-2-007',
+    alerts,
+  },
 ]
 
-const toCourtEventRow = ($cell) => ({
+const toScheduledMove = ($cell) => ({
   name: $cell[0]?.textContent,
   cell: $cell[1]?.textContent,
   property: $cell[2]?.textContent,
@@ -150,7 +216,7 @@ context('Scheduled movements', () => {
           .then(($tableRows) => {
             cy.get($tableRows).its('length').should('eq', 1)
 
-            const rows = Array.from($tableRows).map(($row) => toCourtEventRow($row.cells))
+            const rows = Array.from($tableRows).map(($row) => toScheduledMove($row.cells))
 
             expect(rows[0].name).to.contain('Cob, Bob - G4797UD')
             expect(rows[0].cell).to.eq('1-2-006')
@@ -159,6 +225,87 @@ context('Scheduled movements', () => {
             expect(rows[0].alerts).contains('ACCT open')
             expect(rows[0].reason).eq('Court Appearance')
             expect(rows[0].destination).eq("Aberdeen Sheriff's Court (ABDSHF)")
+          })
+      })
+    })
+  })
+
+  context('Releases', () => {
+    it('should display default message for no releases scheduled for given date', () => {
+      cy.visit('/manage-prisoner-whereabouts/scheduled-moves')
+      cy.get('#no-release-events').contains(`There are no releases arranged for ${today.format('D MMMM YYYY')}`)
+    })
+
+    context('With releases', () => {
+      beforeEach(() => {
+        cy.task('resetTransfersStub')
+        cy.task('stubTransfers', {
+          courtEvents: [],
+          transferEvents: [],
+          releaseEvents,
+        })
+        cy.task('stubPrisonerSearchDetails', prisonerSearchDetails)
+        cy.task('stubPrisonerProperty', propertyResponse)
+      })
+
+      it('should display releases in the table correctly', () => {
+        cy.visit('/manage-prisoner-whereabouts/scheduled-moves')
+
+        cy.get('[data-qa="release-events-table"]')
+          .find('tbody')
+          .find('tr')
+          .then(($tableRows) => {
+            cy.get($tableRows).its('length').should('eq', 1)
+
+            const rows = Array.from($tableRows).map(($row) => toScheduledMove($row.cells))
+
+            expect(rows[0].name).to.contain('Shave, Dave - G3854XD')
+            expect(rows[0].cell).to.eq('1-2-008')
+            expect(rows[0].property).contains('Valuables - Box 14')
+            expect(rows[0].property).contains('Confiscated - Box 15')
+            expect(rows[0].alerts).contains('ACCT open')
+            expect(rows[0].reason).eq('Conditional Release (CJA91) -SH Term>1YR')
+          })
+      })
+    })
+  })
+
+  context('Transfers', () => {
+    it('should display default message for no transfers scheduled for given date', () => {
+      cy.visit('/manage-prisoner-whereabouts/scheduled-moves')
+      cy.get('#no-transfer-events').contains(`There are no transfers arranged for ${today.format('D MMMM YYYY')}`)
+    })
+
+    context('With releases', () => {
+      beforeEach(() => {
+        cy.task('resetTransfersStub')
+        cy.task('stubTransfers', {
+          courtEvents: [],
+          transferEvents,
+          releaseEvents: [],
+        })
+        cy.task('stubPrisonerSearchDetails', prisonerSearchDetails)
+        cy.task('stubPrisonerProperty', propertyResponse)
+      })
+
+      it('should display releases in the table correctly', () => {
+        cy.visit('/manage-prisoner-whereabouts/scheduled-moves')
+
+        cy.get('[data-qa="transfer-events-table"]')
+          .find('tbody')
+          .find('tr')
+          .then(($tableRows) => {
+            cy.get($tableRows).its('length').should('eq', 1)
+
+            const rows = Array.from($tableRows).map(($row) => toScheduledMove($row.cells))
+
+            expect(rows[0].name).to.contain('Shark, Mark - G5966UI')
+            expect(rows[0].cell).to.eq('1-2-007')
+            expect(rows[0].property).contains('Valuables - Box 14')
+            expect(rows[0].property).contains('Confiscated - Box 15')
+            expect(rows[0].alerts).contains('ACCT open')
+            expect(rows[0].reason).eq('Normal Transfer')
+            expect(rows[0].destination).eq('Leeds (HMP)')
           })
       })
     })
