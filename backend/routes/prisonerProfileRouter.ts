fcd6bef1ce2f313aee834952615c7c0d90f378dd
Peter Phillips <peter@greenthistle.co.uk>
2022-01-26 15:29:12 +0000

    SDI-4: 💄 Rename visits details

--- backend/routes/prisonerProfileRouter.ts
+++ backend/routes/prisonerProfileRouter.ts
@@ -111,7 +111,7 @@ const controller = ({
   router.get('/skills', learnerEmployabilitySkills({ paginationService, prisonApi, esweService }))
   router.get('/work-activities', workInsidePrison({ paginationService, prisonApi, esweService }))
   // @ts-expect-error ts-migrate(2345) FIXME: Argument of type '{ prisonApi: any; logError: any;... Remove this comment to see the full error message
-  router.get('/visit-details', prisonerVisits({ prisonApi, logError }))
+  router.get('/visits-details', prisonerVisits({ prisonApi, logError }))
   // @ts-expect-error ts-migrate(2345) FIXME: Argument of type '{ prisonApi: any; logError: any;... Remove this comment to see the full error message
   router.get('/schedule', prisonerSchedule({ prisonApi, logError }))
   router.get(
