b402f1279545ce4fea7f93537afb2520adee0c5a
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-02-15 13:17:17 +0000

    INC-88: Choose random prompts for positive/negative behaviour entries

--- backend/controllers/caseNote.ts
+++ backend/controllers/caseNote.ts
@@ -4,7 +4,7 @@ import { properCaseName } from '../utils'
 
 const getOffenderUrl = (offenderNo) => `/prisoner/${offenderNo}`
 
-export const caseNoteFactory = ({ prisonApi, caseNotesApi }) => {
+export const caseNoteFactory = ({ prisonApi, caseNotesApi, showBehaviourPrompts = false }) => {
   const getOffenderDetails = async (res, offenderNo) => {
     const { firstName, lastName } = await prisonApi.getDetails(res.locals, offenderNo)
 
@@ -61,6 +61,17 @@ export const caseNoteFactory = ({ prisonApi, caseNotesApi }) => {
     }
   }
 
+  const chooseBehaviourPrompts = () => {
+    if (!showBehaviourPrompts) return {}
+
+    return Object.fromEntries(
+      Object.entries(behaviourPrompts).map(([type, prompts]) => {
+        const index = Math.floor(Math.random() * prompts.length)
+        return [type, prompts[index]]
+      })
+    )
+  }
+
   const index = async (req, res) => {
     const { offenderNo } = req.params
     try {
@@ -80,6 +91,7 @@ export const caseNoteFactory = ({ prisonApi, caseNotesApi }) => {
         formValues,
         types,
         subTypes,
+        behaviourPrompts: chooseBehaviourPrompts(),
         homeUrl: `${getOffenderUrl(offenderNo)}/case-notes`,
         caseNotesRootUrl: `/prisoner/${offenderNo}/add-case-note`,
       })
@@ -291,6 +303,53 @@ export const caseNoteFactory = ({ prisonApi, caseNotesApi }) => {
   }
 
   return { index, post, areYouSure, confirm }
+}
+
+const behaviourPrompts = {
+  pos: [
+    {
+      summary: 'Why recognise positive behaviour?',
+      text: 'Verbal praise delivered promptly is the most effective way of reinforcing good behaviour which can lead to long-term changes.',
+    },
+    {
+      summary: 'Noticing improved behaviour',
+      text: 'Noticing and praising improved behaviour is the most effective tool we have to influence long-term behaviour changes.',
+    },
+    {
+      summary: 'What is ‘good’ behaviour?',
+      text: 'Think about a person’s individual background and needs. For some, getting through the day without incident is an improvement and should be noted.',
+    },
+    {
+      summary: 'Noticing differences in behaviour',
+      text: 'Progress means different things for different people. A positive entry could be for an improvement to usual patterns of behaviour.',
+    },
+    {
+      summary: 'Positive case entries and young adults',
+      text: 'Evidence suggests noticing and praising something a prisoner does well is much more likely to lead to long-term changes than only noticing poor behaviour, particularly in younger or less mature prisoners.',
+    },
+  ],
+  neg: [
+    {
+      summary: 'Creating long-term change',
+      text: 'Evidence suggests noticing and praising something a prisoner does well is much more likely to lead to long-term changes than only noticing poor behaviour.',
+    },
+    {
+      summary: 'Cooling down a situation',
+      text: 'Is there opportunity to let the situation cool down? Maybe it’s having a private chat, asking another officer for support, or allowing for some cool-down time.',
+    },
+    {
+      summary: 'Reasons for negative behaviour',
+      text: 'What’s causing this negative behaviour? It’s not always possible, but finding out any background could help decide if a behaviour entry is the best action.',
+    },
+    {
+      summary: 'What happens next?',
+      text: 'Things that might help after making an entry include: asking about circumstances around the behaviour, listening, and then agreeing how they could behave better next time.',
+    },
+    {
+      summary: 'Talking about what happened',
+      text: 'Is there a chance for you or the person’s key worker to talk about what happened? You might be able to agree how to handle things better next time.',
+    },
+  ],
 }
 
 export default { caseNoteFactory }
