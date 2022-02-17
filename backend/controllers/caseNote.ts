798d6da97480dd60a9428f7b777b3b3ae07f9c92
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-02-17 12:35:53 +0000

    INC-231 Give behaviour prompts unique IDs to track viewing in GA

--- backend/controllers/caseNote.ts
+++ backend/controllers/caseNote.ts
@@ -310,49 +310,59 @@ export const caseNoteFactory = ({
   return { index, post, areYouSure, confirm }
 }
 
-const behaviourPrompts = {
+export const behaviourPrompts = {
   pos: [
     {
       summary: 'Why recognise positive behaviour?',
       text: 'Verbal praise delivered promptly is the most effective way of reinforcing good behaviour which can lead to long-term changes.',
+      gaId: 'Positive behaviour prompt: Why recognise positive behaviour?',
     },
     {
       summary: 'Noticing improved behaviour',
       text: 'Noticing and praising improved behaviour is the most effective tool we have to influence long-term behaviour changes.',
+      gaId: 'Positive behaviour prompt: Noticing improved behaviour',
     },
     {
       summary: 'What is ‘good’ behaviour?',
       text: 'Think about a person’s individual background and needs. For some, getting through the day without incident is an improvement and should be noted.',
+      gaId: 'Positive behaviour prompt: What is good behaviour?',
     },
     {
       summary: 'Noticing differences in behaviour',
       text: 'Progress means different things for different people. A positive entry could be for an improvement to usual patterns of behaviour.',
+      gaId: 'Positive behaviour prompt: Noticing differences in behaviour',
     },
     {
       summary: 'Positive case entries and young adults',
       text: 'Evidence suggests noticing and praising something a prisoner does well is much more likely to lead to long-term changes than only noticing poor behaviour, particularly in younger or less mature prisoners.',
+      gaId: 'Positive behaviour prompt: Positive case entries and young adults',
     },
   ],
   neg: [
     {
       summary: 'Creating long-term change',
       text: 'Evidence suggests noticing and praising something a prisoner does well is much more likely to lead to long-term changes than only noticing poor behaviour.',
+      gaId: 'Negative behaviour prompt: Creating long-term change',
     },
     {
       summary: 'Cooling down a situation',
       text: 'Is there opportunity to let the situation cool down? Maybe it’s having a private chat, asking another officer for support, or allowing for some cool-down time.',
+      gaId: 'Negative behaviour prompt: Cooling down a situation',
     },
     {
       summary: 'Reasons for negative behaviour',
       text: 'What’s causing this negative behaviour? It’s not always possible, but finding out any background could help decide if a behaviour entry is the best action.',
+      gaId: 'Negative behaviour prompt: Reasons for negative behaviour',
     },
     {
       summary: 'What happens next?',
       text: 'Things that might help after making an entry include: asking about circumstances around the behaviour, listening, and then agreeing what could improve next time.',
+      gaId: 'Negative behaviour prompt: What happens next?',
     },
     {
       summary: 'Talking about what happened',
       text: 'Is there a chance for you or the person’s key worker to talk about what happened? You might be able to agree how to handle things better next time.',
+      gaId: 'Negative behaviour prompt: Talking about what happened',
     },
   ],
 }
