81eb452f6a68b4f9dfc05111f614ae45c5a7d5b9
Nat Clamp <84783598+natclamp-moj@users.noreply.github.com>
2022-03-31 14:55:25 +0100

    NN-4035: attendance changes files (#2085)
    * NN-4035: attendance changes files
    
    * NN-4035: Tidy up

--- backend/tests/attendanceChanges.test.ts
+++ backend/tests/attendanceChanges.test.ts
@@ -1,30 +1,27 @@
 import attendanceChangeRouter from '../routes/attendanceChangesRouter'
 
 describe('Attendance change router', () => {
-  const prisonApi = {}
-  const whereaboutsApi = {}
-  const oauthApi = {}
   const res = {
     locals: {},
+    render: jest.fn(),
+    redirect: jest.fn(),
+    status: jest.fn(),
+  }
+
+  const whereaboutsApi = {
+    getAttendanceChanges: jest.fn(),
+  }
+  const prisonApi = {
+    getScheduledActivities: jest.fn(),
+    getUserDetailsList: jest.fn(),
   }
 
   let req
   let router
-  let logError
 
   beforeEach(() => {
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'getAttendanceChanges' does not exist on ... Remove this comment to see the full error message
-    whereaboutsApi.getAttendanceChanges = jest.fn()
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'getScheduledActivities' does not exist o... Remove this comment to see the full error message
-    prisonApi.getScheduledActivities = jest.fn()
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'userDetails' does not exist on type '{}'... Remove this comment to see the full error message
-    prisonApi.getUserDetailsList = jest.fn()
-
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'getAttendanceChanges' does not exist on ... Remove this comment to see the full error message
     whereaboutsApi.getAttendanceChanges.mockReturnValue({ changes: [] })
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'getScheduledActivities' does not exist o... Remove this comment to see the full error message
     prisonApi.getScheduledActivities.mockReturnValue([])
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'userDetails' does not exist on type '{}'... Remove this comment to see the full error message
     prisonApi.getUserDetailsList.mockReturnValue([
       {
         username: 'username1',
@@ -32,15 +29,7 @@ describe('Attendance change router', () => {
         lastName: 'Last name',
       },
     ])
-
-    logError = jest.fn()
-
-    // @ts-expect-error ts-migrate(2345) FIXME: Argument of type '{ prisonApi: {}; whereaboutsApi:... Remove this comment to see the full error message
-    router = attendanceChangeRouter({ prisonApi, whereaboutsApi, oauthApi, logError })
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'render' does not exist on type '{ locals... Remove this comment to see the full error message
-    res.render = jest.fn()
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'redirect' does not exist on type '{ loca... Remove this comment to see the full error message
-    res.redirect = jest.fn()
+    router = attendanceChangeRouter({ prisonApi, whereaboutsApi })
 
     req = {
       originalUrl: 'http://localhost',
@@ -52,14 +41,12 @@ describe('Attendance change router', () => {
       },
     }
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'status' does not exist on type '{ locals... Remove this comment to see the full error message
     res.status = jest.fn()
   })
 
   it('should request changes for a given date time range', async () => {
     await router(req, res)
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'getAttendanceChanges' does not exist on ... Remove this comment to see the full error message
     expect(whereaboutsApi.getAttendanceChanges).toHaveBeenCalledWith(
       {},
       expect.objectContaining({
@@ -70,19 +57,16 @@ describe('Attendance change router', () => {
   })
 
   it('should make a request for scheduled activity that has been changed', async () => {
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'getAttendanceChanges' does not exist on ... Remove this comment to see the full error message
     whereaboutsApi.getAttendanceChanges.mockReturnValue({
       changes: [{ eventId: 1 }, { eventId: 1 }, { eventId: 2 }],
     })
 
     await router(req, res)
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'getScheduledActivities' does not exist o... Remove this comment to see the full error message
     expect(prisonApi.getScheduledActivities).toHaveBeenCalledWith({}, { agencyId: 'MDI', eventIds: [1, 2] })
   })
 
   it('should make a request to get user details', async () => {
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'getAttendanceChanges' does not exist on ... Remove this comment to see the full error message
     whereaboutsApi.getAttendanceChanges.mockReturnValue({
       changes: [
         { eventId: 1, changedBy: 'username1', prisonId: 'MDI' },
@@ -93,12 +77,10 @@ describe('Attendance change router', () => {
 
     await router(req, res)
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'userDetails' does not exist on type '{}'... Remove this comment to see the full error message
     expect(prisonApi.getUserDetailsList).toHaveBeenCalledWith({}, ['username1', 'username2'])
   })
 
   it('should return table rows in the right order and format', async () => {
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'getAttendanceChanges' does not exist on ... Remove this comment to see the full error message
     whereaboutsApi.getAttendanceChanges.mockReturnValue({
       changes: [
         {
@@ -120,13 +102,11 @@ describe('Attendance change router', () => {
       ],
     })
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'getScheduledActivities' does not exist o... Remove this comment to see the full error message
     prisonApi.getScheduledActivities.mockReturnValue([
       { eventId: 1, firstName: 'first name 1', lastName: 'last name', comment: 'Wood work', offenderNo: 'A123456' },
       { eventId: 2, firstName: 'first name 2', lastName: 'last name', comment: 'Kitchen', offenderNo: 'A23457' },
     ])
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'userDetails' does not exist on type '{}'... Remove this comment to see the full error message
     prisonApi.getUserDetailsList.mockReturnValue([
       {
         firstName: 'Peter',
@@ -137,7 +117,6 @@ describe('Attendance change router', () => {
 
     await router(req, res)
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'render' does not exist on type '{ locals... Remove this comment to see the full error message
     expect(res.render).toHaveBeenCalledWith(
       'attendanceChanges.njk',
       expect.objectContaining({
@@ -176,16 +155,13 @@ describe('Attendance change router', () => {
     req.query = {}
     await router(req, res)
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'redirect' does not exist on type '{ loca... Remove this comment to see the full error message
     expect(res.redirect).toHaveBeenCalledWith('/manage-prisoner-whereabouts/attendance-reason-statistics')
   })
 
   it('should render page when there are no changes', async () => {
+    res.redirect = jest.fn()
     await router(req, res)
-
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'redirect' does not exist on type '{ loca... Remove this comment to see the full error message
     expect(res.redirect).toHaveBeenCalledTimes(0)
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'render' does not exist on type '{ locals... Remove this comment to see the full error message
     expect(res.render).toHaveBeenCalledWith(
       'attendanceChanges.njk',
       expect.objectContaining({
@@ -196,7 +172,6 @@ describe('Attendance change router', () => {
   })
 
   it('should only show changes from the requested agency', async () => {
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'getAttendanceChanges' does not exist on ... Remove this comment to see the full error message
     whereaboutsApi.getAttendanceChanges.mockReturnValue({
       changes: [
         {
@@ -218,18 +193,15 @@ describe('Attendance change router', () => {
       ],
     })
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'getScheduledActivities' does not exist o... Remove this comment to see the full error message
     prisonApi.getScheduledActivities.mockReturnValue([
       { eventId: 1, firstName: 'first name 1', lastName: 'last name', comment: 'Wood work', offenderNo: 'A123456' },
       { eventId: 2, firstName: 'first name 2', lastName: 'last name', comment: 'Kitchen', offenderNo: 'A23457' },
     ])
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'userDetails' does not exist on type '{}'... Remove this comment to see the full error message
     prisonApi.getUserDetailsList.mockReturnValue([{ firstName: 'Steve', lastName: 'Walsh', username: 'username' }])
 
     await router(req, res)
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'render' does not exist on type '{ locals... Remove this comment to see the full error message
     expect(res.render).toHaveBeenCalledWith(
       'attendanceChanges.njk',
       expect.objectContaining({
