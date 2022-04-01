40bb3530775053e34b02aa9d2d59369e4c931c1c
Nat Clamp <84783598+natclamp-moj@users.noreply.github.com>
2022-04-01 14:47:29 +0100

    NN-4037: Remove expect-error comments (#2088)

--- backend/tests/activityListController.test.ts
+++ backend/tests/activityListController.test.ts
@@ -3,72 +3,74 @@ import { factory } from '../controllers/controller'
 import { makeResetError, makeError, makeResetErrorWithStack } from './helpers'
 
 describe('Activity list controller', () => {
-  const activityListService = {}
+  const activityListService = {
+    getActivityList: jest.fn(),
+  }
+  const houseblockListService = {}
+  const attendanceService = {}
+  const offenderLoader = {}
+  const csvParserService = {}
+  const offenderActivitesService = {}
+  const caseNotesApi = {}
   let logError
   let getActivityListController
 
-  const req = { originalUrl: 'http://localhost' }
-  const res = { locals: {} }
+  const req = { originalUrl: 'http://localhost', query: {} }
+  const res = { locals: {}, json: jest.fn(), end: jest.fn(), status: jest.fn() }
 
   beforeEach(() => {
     logError = jest.fn()
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'getActivityList' does not exist on type ... Remove this comment to see the full error message
     activityListService.getActivityList = jest.fn()
-    // @ts-expect-error ts-migrate(2345) FIXME: Argument of type '{ activityListService: {}; logEr... Remove this comment to see the full error message
-    getActivityListController = factory({ activityListService, logError }).getActivityList
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'query' does not exist on type '{ origina... Remove this comment to see the full error message
+    getActivityListController = factory({
+      activityListService,
+      houseblockListService,
+      attendanceService,
+      offenderLoader,
+      csvParserService,
+      offenderActivitesService,
+      caseNotesApi,
+      logError,
+    }).getActivityList
+
     req.query = {
       agencyId: 'LEI',
       locationId: 1,
       timeSlot: 'AM',
       date: '10/12/2020',
     }
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'json' does not exist on type '{ locals: ... Remove this comment to see the full error message
     res.json = jest.fn()
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'end' does not exist on type '{ locals: {... Remove this comment to see the full error message
     res.end = jest.fn()
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'status' does not exist on type '{ locals... Remove this comment to see the full error message
     res.status = jest.fn()
   })
   describe('Error handling', () => {
     it('should log timeout error with ECONNRESET code', async () => {
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'getActivityList' does not exist on type ... Remove this comment to see the full error message
       activityListService.getActivityList.mockRejectedValue(makeResetError())
 
       await getActivityListController(req, res)
 
       expect(logError.mock.calls.length).toBe(1)
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'json' does not exist on type '{ locals: ... Remove this comment to see the full error message
       expect(res.json.mock.calls.length).toBe(1)
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'end' does not exist on type '{ locals: {... Remove this comment to see the full error message
       expect(res.end).toHaveBeenCalled()
     })
 
     it('should log timeout error with timeout in stack', async () => {
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'getActivityList' does not exist on type ... Remove this comment to see the full error message
       activityListService.getActivityList.mockRejectedValue(makeResetErrorWithStack())
 
       await getActivityListController(req, res)
 
       expect(logError.mock.calls.length).toBe(1)
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'json' does not exist on type '{ locals: ... Remove this comment to see the full error message
       expect(res.json.mock.calls.length).toBe(1)
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'end' does not exist on type '{ locals: {... Remove this comment to see the full error message
       expect(res.end).toHaveBeenCalled()
     })
 
     it('should log non-timeout error', async () => {
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'getActivityList' does not exist on type ... Remove this comment to see the full error message
       activityListService.getActivityList.mockRejectedValue(makeError('status', 403))
 
       await getActivityListController(req, res)
 
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'json' does not exist on type '{ locals: ... Remove this comment to see the full error message
       expect(res.json.mock.calls.length).toBe(0)
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'status' does not exist on type '{ locals... Remove this comment to see the full error message
       expect(res.status).toHaveBeenCalledWith(403)
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'end' does not exist on type '{ locals: {... Remove this comment to see the full error message
       expect(res.end).toHaveBeenCalled()
     })
   })
--- backend/tests/activityLocations.test.ts
+++ backend/tests/activityLocations.test.ts
@@ -4,25 +4,22 @@ import { makeError, makeResetError, makeResetErrorWithStack } from './helpers'
 describe('Activity locations', () => {
   let getActivityLocations
   let logError
-  const prisonApi = {}
-  const req = { originalUrl: 'http://localhost' }
-  const res = { locals: {} }
+  const prisonApi = {
+    searchActivityLocations: jest.fn(),
+  }
+  const req = { originalUrl: 'http://localhost', query: {} }
+  const res = { locals: {}, json: jest.fn(), end: jest.fn(), status: jest.fn() }
 
   beforeEach(() => {
     logError = jest.fn()
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'searchActivityLocations' does not exist ... Remove this comment to see the full error message
     prisonApi.searchActivityLocations = jest.fn().mockResolvedValue({ location: 'test' })
 
     getActivityLocations = getActivityLocationsFactory({ prisonApi, logError }).getActivityLocations
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'json' does not exist on type '{ locals: ... Remove this comment to see the full error message
     res.json = jest.fn()
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'end' does not exist on type '{ locals: {... Remove this comment to see the full error message
     res.end = jest.fn()
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'status' does not exist on type '{ locals... Remove this comment to see the full error message
     res.status = jest.fn()
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'query' does not exist on type '{ origina... Remove this comment to see the full error message
     req.query = {
       agencyId: 'LEI',
       timeSlot: 'AM',
@@ -33,14 +30,11 @@ describe('Activity locations', () => {
   it('should make a call to get locations for activity using the correct parameters', async () => {
     await getActivityLocations(req, res)
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'searchActivityLocations' does not exist ... Remove this comment to see the full error message
     expect(prisonApi.searchActivityLocations).toHaveBeenCalledWith({}, 'LEI', '2020-12-10', 'AM')
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'json' does not exist on type '{ locals: ... Remove this comment to see the full error message
     expect(res.json).toHaveBeenCalledWith({ location: 'test' })
   })
 
   it('should log API errors', async () => {
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'searchActivityLocations' does not exist ... Remove this comment to see the full error message
     prisonApi.searchActivityLocations.mockRejectedValue(new Error('test'))
     await getActivityLocations(req, res)
 
@@ -48,55 +42,40 @@ describe('Activity locations', () => {
   })
 
   it('should not log connection reset API errors', async () => {
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'searchActivityLocations' does not exist ... Remove this comment to see the full error message
     prisonApi.searchActivityLocations.mockRejectedValue(makeResetError())
     await getActivityLocations(req, res)
 
     expect(logError.mock.calls.length).toBe(0)
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'status' does not exist on type '{ locals... Remove this comment to see the full error message
     expect(res.status.mock.calls.length).toBe(0)
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'json' does not exist on type '{ locals: ... Remove this comment to see the full error message
     expect(res.json).toHaveBeenCalledWith({ error: true })
   })
 
   it('should not log connection reset API errors with Timout in stack', async () => {
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'searchActivityLocations' does not exist ... Remove this comment to see the full error message
     prisonApi.searchActivityLocations.mockRejectedValue(makeResetErrorWithStack())
     await getActivityLocations(req, res)
 
     expect(logError.mock.calls.length).toBe(0)
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'status' does not exist on type '{ locals... Remove this comment to see the full error message
     expect(res.status.mock.calls.length).toBe(0)
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'json' does not exist on type '{ locals: ... Remove this comment to see the full error message
     expect(res.json).toHaveBeenCalledWith({ error: true })
   })
 
   it('should respond with the correct status codes', async () => {
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'searchActivityLocations' does not exist ... Remove this comment to see the full error message
     prisonApi.searchActivityLocations.mockRejectedValue(makeError('status', 403))
     await getActivityLocations(req, res)
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'status' does not exist on type '{ locals... Remove this comment to see the full error message
     expect(res.status).toHaveBeenCalledWith(403)
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'end' does not exist on type '{ locals: {... Remove this comment to see the full error message
     expect(res.end).toHaveBeenCalled()
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'searchActivityLocations' does not exist ... Remove this comment to see the full error message
     prisonApi.searchActivityLocations.mockRejectedValue(makeError('response', { status: 404 }))
     await getActivityLocations(req, res)
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'status' does not exist on type '{ locals... Remove this comment to see the full error message
     expect(res.status).toHaveBeenCalledWith(404)
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'end' does not exist on type '{ locals: {... Remove this comment to see the full error message
     expect(res.end).toHaveBeenCalled()
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'searchActivityLocations' does not exist ... Remove this comment to see the full error message
     prisonApi.searchActivityLocations.mockRejectedValue(new Error())
     await getActivityLocations(req, res)
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'status' does not exist on type '{ locals... Remove this comment to see the full error message
     expect(res.status).toHaveBeenCalledWith(500)
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'end' does not exist on type '{ locals: {... Remove this comment to see the full error message
     expect(res.end).toHaveBeenCalled()
   })
 })
