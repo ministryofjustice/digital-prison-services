b9dc89c7e94884b4a61d2685d3d44074824d1e0e
Natalie Clamp <natalie.clamp@digital.justice.gov.uk>
2022-05-09 13:09:26 +0100

    NN-4019: Add test to maintain redis v3

--- backend/setupWebSession.ts
+++ backend/setupWebSession.ts
@@ -19,14 +19,6 @@ export default () => {
       port,
       password,
       tls: config.app.production ? {} : false,
-      socket: {
-        reconnectStrategy: (attempts: number) => {
-          // Exponential back off: 20ms, 40ms, 80ms..., capped to retry every 30 seconds
-          const nextDelay = Math.min(2 ** attempts * 20, 30000)
-          logger.info(`Retry Redis connection attempt: ${attempts}, next attempt in: ${nextDelay}ms`)
-          return nextDelay
-        },
-      },
     })
 
     client.on('error', (e: Error) => logger.error('Redis client error', e))
