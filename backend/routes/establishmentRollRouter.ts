84ba118e525869189ed54b290969b36a748229ea
Natalie Clamp <natalie.clamp@digital.justice.gov.uk>
2022-05-10 17:04:29 +0100

    NN-4113: Remove blocker to build

--- backend/routes/establishmentRollRouter.ts
+++ backend/routes/establishmentRollRouter.ts
@@ -18,7 +18,6 @@ const controller = ({ oauthApi, prisonApi, systemOauthClient, logError, incentiv
   router.get('/in-reception', inReceptionController({ movementsService, logError }))
   // @ts-expect-error ts-migrate(2345) FIXME: Argument of type '{ prisonApi: any; movementsServi... Remove this comment to see the full error message
   router.get('/en-route', enRouteController({ prisonApi, movementsService, logError }))
-  // @ts-expect-error ts-migrate(2345) FIXME: Argument of type '{ movementsService: { getMovemen... Remove this comment to see the full error message
   router.get('/:livingUnitId/currently-out', currentlyOutController({ movementsService, logError }))
   router.get('/total-currently-out', totalCurrentlyOutController({ movementsService, logError }))
   router.get('/in-today', inTodayController({ movementsService, logError }))
