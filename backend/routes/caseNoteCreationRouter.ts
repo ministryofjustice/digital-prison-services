8b2f208f4597e6baace779bdfb8d4bee921df48a
Tim Wright <wright.tim.j@gmail.com>
2022-05-18 11:10:03 +0100

    NN-4065 changes to enable a case note to be added (#2152)
    * changes to enable a case note to be added
    
    * fix test, added POM role to client token too

--- backend/routes/caseNoteCreationRouter.ts
+++ backend/routes/caseNoteCreationRouter.ts
@@ -3,8 +3,14 @@ import { caseNoteFactory } from '../controllers/caseNote'
 
 const router = express.Router({ mergeParams: true })
 
-const controller = ({ prisonApi, caseNotesApi }) => {
-  const { index, post, areYouSure, confirm } = caseNoteFactory({ prisonApi, caseNotesApi })
+const controller = ({ prisonApi, caseNotesApi, oauthApi, systemOauthClient, restrictedPatientApi }) => {
+  const { index, post, areYouSure, confirm } = caseNoteFactory({
+    prisonApi,
+    caseNotesApi,
+    oauthApi,
+    systemOauthClient,
+    restrictedPatientApi,
+  })
 
   router.get('/', index)
   router.post('/', post)
