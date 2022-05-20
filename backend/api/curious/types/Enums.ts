6e61686808186fc2e1baa37e0e1c59ae09c2bae8
mons <mons.kapinga@digital.justice.gov.uk>
2022-05-20 09:18:44 +0100

    ESWE-287_feature_flag_neurodivergence_indicator_repair (#2158)
    * ESWE-287: feature flag neurodiversity indicator refactor to restrict access to accelerated prisons
    
    * ESWE-287: feature flag neurodiversity indicator refactor to restrict access to accelerated prisons
    
    * ESWE-287: feature flag neurodiversity indicator refactor to restrict access to accelerated prisons
    
    * ESWE-287: feature flag neurodiversity indicator refactor to restrict access to accelerated prisons
    
    * ESWE-287: feature flag neurodiversity indicator refactor to restrict access to accelerated prisons
    
    * ESWE-287: feature flag neurodiversity indicator refactor to restrict access to accelerated prisons

--- backend/api/curious/types/Enums.ts
+++ backend/api/curious/types/Enums.ts
@@ -70,13 +70,13 @@ export enum NeurodivergenceAssessed {
 export enum NeurodivergenceSupport {
   Reading = 'Reading Support',
   WritingSupport = 'Writing Support',
-  SocialandinteractionSupport = 'Social and interaction Support',
+  SocialAndInteractionSupport = 'Social and interaction Support',
   Communications = 'Communications',
   VisualSupport = 'Visual Support',
   AuditorySupport = 'Auditory Support',
   OrganisationSupport = 'Organisation Support',
   MemorySupport = 'Memory Support',
-  NoidentifiedNeurodoversityNeed = 'No identified Neurodoversity Need',
-  TimemanagementSupport = 'Time management Support',
-  NoidentifiedSupportRequired = 'No identified Support Required',
+  NoIdentifiedNeurodiversityNeed = 'No identified Neurodiversity Need',
+  TimeManagementSupport = 'Time Management Support',
+  NoIdentifiedSupportRequired = 'No identified Support Required',
 }
