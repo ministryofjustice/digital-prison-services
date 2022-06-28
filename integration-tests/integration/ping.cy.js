97d5314886555df6c6cf635255d8623266bcf377
tim <wright.tim.j@gmail.com>
2022-06-28 14:03:26 +0100

    NN-4211 cypress 10

--- integration-tests/integration/ping.cy.js
+++ integration-tests/integration/ping.cy.js
@@ -1,6 +1,6 @@
 context('Ping response correctly', () => {
   it('Returns pong', () => {
-    cy.request('/ping').then(response => {
+    cy.request('/ping').then((response) => {
       expect(response.body).to.equal('pong')
     })
   })
