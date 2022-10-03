42b829bfa6885e263f4bc699669f9b23cf08415e
tim <wright.tim.j@gmail.com>
2022-10-03 16:19:43 +0100

    NN-4417 pr comments, remove redirect page

--- backend/controllers/deprecatedUrlPage.ts
+++
@@ -1,7 +0,0 @@
-export default (redirectUrlPrefix) => async (req, res) => {
-  const targetUrl = req.originalUrl.substr(redirectUrlPrefix.length) || '/'
-
-  return res.render('deprecatedUrlPage.njk', {
-    originalUrl: targetUrl,
-  })
-}
