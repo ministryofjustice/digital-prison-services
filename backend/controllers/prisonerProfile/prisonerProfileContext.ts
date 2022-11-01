68edb836b989ca3c3e3d44f6240805fa2527ea74
Peter Phillips <peter@greenthistle.co.uk>
2022-11-01 09:42:52 +0000

    SDI-422: ✨ Allow restricted patients calls to be feature flagged off

--- backend/controllers/prisonerProfile/prisonerProfileContext.ts
+++ backend/controllers/prisonerProfile/prisonerProfileContext.ts
@@ -1,3 +1,5 @@
+import config from '../../config'
+
 const getContext = async ({ offenderNo, res, req, oauthApi, systemOauthClient, restrictedPatientApi }) => {
   if (res.locals.user === undefined) {
     return { context: res.locals, overrideAccess: false }
@@ -7,9 +9,9 @@ const getContext = async ({ offenderNo, res, req, oauthApi, systemOauthClient, r
   } = res.locals
 
   const { username } = req.session.userDetails
-  const userRoles = oauthApi.userRoles(res.locals)
+  const userRoles = await oauthApi.userRoles(res.locals)
 
-  if (userRoles.map((userRole) => userRole.roleCode).includes('POM')) {
+  if (config.app.pomRestrictedPatientsEnabled && userRoles.map((userRole) => userRole.roleCode).includes('POM')) {
     const isRestrictedPatient = await restrictedPatientApi.isCaseLoadRestrictedPatient(
       res.locals,
       offenderNo,
