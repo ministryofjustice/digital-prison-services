9e0c99829e2df8efd3ca063c982826b848b360d2
James Grant <james.grant@digital.justice.gov.uk>
2022-11-30 10:32:41 +0000

    NN-3918-update-adjudications-tile

--- backend/controllers/homepage/homepage.ts
+++ backend/controllers/homepage/homepage.ts
@@ -193,8 +193,8 @@ const getTasks = ({ activeCaseLoadId, locations, staffId, whereaboutsConfig, key
     },
     {
       id: 'manage-adjudications',
-      heading: 'Place a prisoner on report',
-      description: 'Place a prisoner on report after an incident and view all of your completed reports.',
+      heading: 'Adjudications',
+      description: 'Place a prisoner on report after an incident, view reports and manage adjudications.',
       href: manageAdjudications.ui_url,
       enabled: () =>
         manageAdjudications.ui_url && manageAdjudications.enabled_prisons.split(',').includes(activeCaseLoadId),
