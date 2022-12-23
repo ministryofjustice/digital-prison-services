a7ad8fbe6b610c896689811c1b7c3880419297ff
tim <wright.tim.j@gmail.com>
2022-12-23 09:39:13 +0000

    NN-4532 add latest session token logic from template

--- backend/setupWebSession.ts
+++ backend/setupWebSession.ts
@@ -44,5 +44,13 @@ export default () => {
       },
     })
   )
+
+  // Update a value in the cookie so that the set-cookie will be sent.
+  // Only changes every minute so that it's not sent with every request.
+  router.use((req, res, next) => {
+    req.session.nowInMinutes = Math.floor(Date.now() / 60e3)
+    next()
+  })
+
   return router
 }
