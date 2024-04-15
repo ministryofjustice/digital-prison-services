71c5c5c8f9c06b0d0e29fe6a67369d2e419ce15a
jimbali <40831617+jimbali@users.noreply.github.com>
2024-04-15 15:42:47 +0100

    [MAP-948] Delete cell moves code (#2724)
    chore: Delete cell moves code
    
    Change Someone's Cell now exists in its own standalone project. All existing
    code has been deleted, except for the "Change Someone's Cell Has Moved" page.
    
    [MAP-948]

--- backend/controllers/cellMove/cellMovesHasMovedPage.ts
+++ backend/controllers/cellMove/cellMovesHasMovedPage.ts
@@ -1,8 +1,6 @@
 import config from '../../config'
 
 export default (req, res) => {
-  const { activeCaseLoad } = res.locals.user
-
   return res.render('cellMove/cellMovesHasMovedPage', {
     changeSomeonesCellUrl: config.apis.changeSomeonesCell.ui_url,
   })
