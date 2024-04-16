c8025eb5ad79781a48744c17b6e4479fd87ff18d
Dani Burnley <dan@danops.uk>
2024-04-16 09:26:29 +0100

    CDPS-708: Redirect all traffic regardless of caseloads to the new homepage

--- backend/controllers/homepage/homepageRedirect.ts
+++ backend/controllers/homepage/homepageRedirect.ts
@@ -1,36 +1,8 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express'
 import config from '../../config'
 
-const isRedirectEnabled = (activeCaseLoadId: string): boolean => {
-  const redirectUrl = config.app.homepageRedirect.url
-  const redirectDate = config.app.homepageRedirect.enabledDate
-  const caseloadScheduled = config.app.homepageRedirect.scheduleRedirectForPrisons
-    ?.split(',')
-    ?.includes(activeCaseLoadId)
-
-  return redirectUrl && ((caseloadScheduled && redirectDate < Date.now()) || !caseloadScheduled)
-}
-
-const isRedirectCaseLoad = (activeCaseLoadId: string): boolean => {
-  return config.app.homepageRedirect.enabledPrisons?.split(',')?.includes(activeCaseLoadId)
-}
-
-export default (handler: RequestHandler) =>
+// This now redirects all users to the new homepage regardless of caseload
+export default (_handler: RequestHandler) =>
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    const { activeCaseLoadId } = req.session.userDetails
-
-    const exemptions = config.app.homepageRedirect.exemptions.split(',')
-
-    if (exemptions.includes(activeCaseLoadId)) {
-      return handler(req, res, next)
-    }
-
-    const redirectEnabled = isRedirectEnabled(activeCaseLoadId)
-    const redirectCaseload = isRedirectCaseLoad(activeCaseLoadId)
-
-    if (redirectEnabled && redirectCaseload) {
-      return res.redirect(`${config.app.homepageRedirect.url}`)
-    }
-
-    return handler(req, res, next)
+    return res.redirect(`${config.app.homepageRedirect.url}`)
   }
