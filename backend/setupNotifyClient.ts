040c4d9dabc2bf82badc1e1f109e2dabb295c7ab
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2025-01-13 14:53:01 +0000

    BAVL-550 removing now redundant legacy BVLS email code/configuration. (#2818)

--- backend/setupNotifyClient.ts
+++
@@ -1,9 +0,0 @@
-import { NotifyClient } from 'notifications-node-client'
-import config from './config'
-
-// @ts-expect-error ts-migrate(2339) FIXME: Property 'notifyClient' does not exist on type '{ ... Remove this comment to see the full error message
-const notifyClient = config.notifyClient.enabled ? new NotifyClient(config.notifications.notifyKey) : { sendEmail() {} }
-
-export default {
-  notifyClient,
-}
