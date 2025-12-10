b842f1ba249733209cfb2823f86d60595cea6a89
Natalie Rolph <natalie.rolph@justice.gov.uk>
2025-12-10 14:12:16 +0000

    Include npm-script-allowlist

---
+++ .allowed-scripts.mjs
@@ -0,0 +1,12 @@
+import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
+
+export default configureAllowedScripts({
+   allowlist: {
+  "node_modules/@parcel/watcher@2.5.1": "ALLOW",
+  "node_modules/core-js@2.6.12": "ALLOW",
+  "node_modules/cypress@13.17.0": "ALLOW",
+  "node_modules/dtrace-provider@0.8.8": "ALLOW",
+  "node_modules/fsevents@2.3.3": "ALLOW",
+  "node_modules/react-final-form@4.1.0": "ALLOW"
+},
+})
