a443cb6815e4cf63009877a6915d000f839b75d5
Natalie Rolph <natalie.rolph@justice.gov.uk>
2025-12-10 15:21:03 +0000

    Can ignore core-js

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,7 +3,7 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
    allowlist: {
   "node_modules/@parcel/watcher@2.5.1": "ALLOW",
-  "node_modules/core-js@2.6.12": "ALLOW",
+  "node_modules/core-js@2.6.12": "FORBID",
   "node_modules/cypress@13.17.0": "ALLOW",
   "node_modules/dtrace-provider@0.8.8": "ALLOW",
   "node_modules/fsevents@2.3.3": "ALLOW",
