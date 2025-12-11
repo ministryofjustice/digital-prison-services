0c34d7b7ca3d02b9599e67772b12b0507fdd5b10
Nat Rolph <84783598+natclamp-moj@users.noreply.github.com>
2025-12-11 10:58:02 +0000

    Update axios dependency to resolve security issue (#3046)

--- webpack.config.js
+++ webpack.config.js
@@ -97,7 +97,7 @@ module.exports = {
           },
           // Process JS with Babel.
           {
-            test: /\.(js|jsx|mjs)$/,
+            test: /\.(js|jsx|mjs|cjs)$/,
             loader: require.resolve('babel-loader'),
             options: {
               // This is a feature of `babel-loader` for webpack (not Babel itself).
@@ -182,7 +182,7 @@ module.exports = {
             // its runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs|cjs)$/, /\.html$/, /\.json$/],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
