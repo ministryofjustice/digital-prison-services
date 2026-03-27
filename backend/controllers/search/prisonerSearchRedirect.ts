41afd63f4f6b52e12f5ea681e52185818a46e8c3
Ewan-Donovan <ewan.donovan@digital.justice.gov.uk>
2026-03-27 15:01:56 +0000

    Remove

--- backend/controllers/search/prisonerSearchRedirect.ts
+++ backend/controllers/search/prisonerSearchRedirect.ts
@@ -1,8 +1,19 @@
 import config from '../../config'
+import qs from 'querystring'
 
-export default () => {
-  return (req, res) => {
-    const query = req.url.split('?')[1]
-    return res.redirect(`${config.app.homepageRedirect.url}/prisoner-search${query ? `?${query}` : ''}`)
-  }
+export const prisonerSearchGetRedirect = (req, res) => {
+  const query = req.url.split('?')[1]
+  return res.redirect(`${config.app.homepageRedirect.url}/prisoner-search${query ? `?${query}` : ''}`)
+}
+
+export const prisonerSearchPostRedirect = (req, res) => {
+  const { alerts, ...queries } = req.query
+
+  return res.redirect(
+    `${req.baseUrl}?${qs.stringify({
+      ...queries,
+      ...(alerts ? { 'alerts[]': alerts } : {}),
+      sortFieldsWithOrder: req.body.sortFieldsWithOrder,
+    })}`
+  )
 }
--- backend/routes/globalSearchRouter.ts
+++ backend/routes/globalSearchRouter.ts
@@ -2,11 +2,7 @@ import express from 'express'
 import globalSearchRedirect from '../controllers/search/globalSearchRedirect'
 
 const router = express.Router()
+router.get('/', globalSearchRedirect({ path: 'global-search' }))
+router.get('/results', globalSearchRedirect({ path: 'global-search/results' }))
 
-const controller = () => {
-  router.get('/', globalSearchRedirect({ path: 'global-search' }))
-  router.get('/results', globalSearchRedirect({ path: 'global-search/results' }))
-  return router
-}
-
-export default () => controller()
+export default router
